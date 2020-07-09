ff042d41fc950a9533a93f0c17089025e5d707c6
jinyanwei <jinyanwei@onetarget.cn>
2020-07-09 18:39:48 +0800

    el-link的demo

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
   <div id="app">
+    <el-link href="http://www.baidu.com" target="_blank" icon="el-icon-edit">默认链接</el-link>
+    <el-link type="primary" icon="el-icon-share">主要链接</el-link>
+    <el-link type="success" :underline="false">成功链接</el-link>
+    <el-link type="warning" :underline="false">警告链接</el-link>
+    <el-link type="danger" :underline="false">危险链接</el-link>
   </div>
 </template>
 
 <script>
-export default {
-};
+  export default {};
 </script>
 
 <style lang="less">
