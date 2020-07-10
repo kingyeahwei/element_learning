6ba3cf59ef908d390c6ccf0da463623595d6423b
jinyanwei <jinyanwei@onetarget.cn>
2020-07-10 14:34:16 +0800

    input demo

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,27 @@
 <template>
   <div id="app">
     <div class="demo-input-size">
-      <el-input v-model="input1"
-                placeholder="请输入内容"
-                suffix-icon="el-icon-date"
-      ></el-input>
-      <el-input v-model="input2"
-                placeholder="请输入内容"
-                size="medium"
-                suffix-icon="el-icon-date"
-      ></el-input>
-      <el-input v-model="input3"
-                suffix-icon="el-icon-date"
-                size="small"
-                placeholder="请输入内容">
-      </el-input>
-      <el-input v-model="input4"
-                placeholder="请输入内容"
-                suffix-icon="el-icon-date"
-                size="mini"
-      ></el-input>
+      <el-input v-model="input1" placeholder="请输入内容" suffix-icon="el-icon-date"></el-input>
+      <el-input v-model="input2" placeholder="请输入内容" size="medium" suffix-icon="el-icon-date"></el-input>
+      <el-input v-model="input3" suffix-icon="el-icon-date" size="small" placeholder="请输入内容"></el-input>
+      <el-input v-model="input4" placeholder="请输入内容" suffix-icon="el-icon-date" size="mini"></el-input>
     </div>
   </div>
 </template>
 
 <script>
-  export default {
-    name: "app",
-    data() {
-      return {
-        input1: "",
-        input2: "",
-        input3: "",
-        input4: "",
-      };
-    },
-    methods: {}
-  };
+export default {
+  name: "app",
+  data() {
+    return {
+      input1: "",
+      input2: "",
+      input3: "",
+      input4: ""
+    };
+  },
+  methods: {}
+};
 </script>
 
 <style lang="less">
