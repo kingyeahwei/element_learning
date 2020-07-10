00bbcbcd5b7f35fe1bcdb51757d3db01618fa025
jinyanwei <jinyanwei@onetarget.cn>
2020-07-10 18:37:37 +0800

    input 自定义模板

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,21 @@
 <template>
   <div id="app">
-    <el-col :span="12" align="center">
-      <div class="sub-title">激活即列出输入的建议</div>
-      <el-autocomplete
-        class="inline-input"
-        v-model="state1"
-        :fetch-suggestions="querySearch"
-        placeholder="请输入内容"
-        @select="handleSelect">
-      </el-autocomplete>
-    </el-col>
-    <el-col :span="12" align="center">
-      <div class="sub-title">输入后匹配输入建议</div>
-      <el-autocomplete
-        class="inline-input"
-        v-model="state2"
-        :fetch-suggestions="querySearch"
-        placeholder="请输入内容"
-        :trigger-on-focus="false"
-        @select="handleSelect"></el-autocomplete>
-    </el-col>
+    <el-autocomplete
+      popper-class="my-autocomplete"
+      class="inline-input"
+      v-model="state"
+      :fetch-suggestions="querySearch"
+      placeholder="请输入内容"
+      @select="handleSelect">
+      <i
+        class="el-icon-edit el-input__icon"
+        slot="suffix"
+        @click="handleIconClick"></i>
+      <template slot-scope="{item}">
+        <div class="name">{{item.value}}</div>
+        <span class="addr">{{item.address}}</span>
+      </template>
+    </el-autocomplete>
   </div>
 </template>
 
@@ -29,8 +25,7 @@
     data() {
       return {
         restaurants: [],
-        state1: '',
-        state2: ''
+        state: ''
       };
     },
     methods: {
@@ -99,6 +94,9 @@
       },
       handleSelect(item) {
         console.log(item);
+      },
+      handleIconClick(ev) {
+        console.log(ev);
       }
     },
     mounted() {
@@ -108,4 +106,24 @@
 </script>
 
 <style lang="less">
+  .my-autocomplete {
+    li {
+      line-height: normal;
+      padding: 7px;
+
+      .name {
+        text-overflow: ellipsis;
+        overflow: hidden;
+      }
+
+      .addr {
+        font-size: 12px;
+        color: #b4b4b4;
+      }
+
+      .highlighted .addr {
+        color: #ddd;
+      }
+    }
+  }
 </style>
