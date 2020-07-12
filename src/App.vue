5653a8094c8221acc48d7a404e9a27729a4bb19e
jinyanwei <jinyanwei@live.com>
2020-07-13 00:05:38 +0800

    分组

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,9 @@
 <template>
   <div id="app">
     <el-select v-model="value" placeholder="请选择">
-      <el-option
-        v-for="item in cities"
-        :key="item.value"
-        :label="item.label"
-        :value="item.value">
-        <span style="float: left;">{{item.label}}</span>
-        <span style="float: right; color: #8492a6; font-size: 13px">{{item.value}}</span>
-      </el-option>
+      <el-option-group v-for="group in options" :key="group.label" :label="group.label">
+        <el-option v-for="item in group.options" :key="item.value" :label="item.label" :value="item.value"></el-option>
+      </el-option-group>
     </el-select>
   </div>
 </template>
@@ -18,32 +13,43 @@
     name: "app",
     data: function () {
       return {
-        cities: [
+        options: [
           {
-            value: 'Beijing',
-            label: '北京'
-          }, {
-            value: 'Shanghai',
-            label: '上海'
-          }, {
-            value: 'Nanjing',
-            label: '南京'
-          }, {
-            value: 'Chengdu',
-            label: '成都'
-          }, {
-            value: 'Shenzhen',
-            label: '深圳'
-          }, {
-            value: 'Guangzhou',
-            label: '广州'
+            label: '热门城市',
+            options: [
+              {
+                value: 'Shanghai',
+                label: '上海'
+              },
+              {
+                value: 'Beijing',
+                label: '北京'
+              }
+            ]
+          },
+          {
+            label: '城市名',
+            options: [
+              {
+                value: 'Chengdu',
+                label: '成都'
+              }, {
+                value: 'Shenzhen',
+                label: '深圳'
+              }, {
+                value: 'Guangzhou',
+                label: '广州'
+              }, {
+                value: 'Dalian',
+                label: '大连'
+              }
+            ]
           }
         ],
         value: "",
       };
     },
-    methods: {
-    },
+    methods: {},
     mounted() {
     }
   };
