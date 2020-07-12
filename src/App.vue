c4d175802d6020430afaf2ae617df4f707c2d24d
jinyanwei <jinyanwei@live.com>
2020-07-12 23:32:09 +0800

    select

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <div id="app">
-    <el-input-number
-      v-model="num"
-      controls-position="right"
-      @change="handleChange"
-      :min="1"
-      :max="10"
-    ></el-input-number>
+    <el-select
+      v-model="value"
+      placeholder="请选择">
+      <el-option
+        v-for="item in options"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value"
+      ></el-option>
+    </el-select>
   </div>
 </template>
 
@@ -15,13 +18,30 @@
     name: "app",
     data: function () {
       return {
-        num: 1,
+        options: [
+          {
+            value: '选项1',
+            label: '黄金糕'
+          }, {
+            value: '选项2',
+            label: '双皮奶'
+          }, {
+            value: '选项3',
+            label: '蚵仔煎'
+          }, {
+            value: '选项4',
+            label: '龙须面'
+          }, {
+            value: '选项5',
+            label: '北京烤鸭'
+          }
+        ],
+        value: ""
       };
     },
     methods: {
-      handleChange(value) {
-        console.log(value)
-      }
+    },
+    mounted() {
     }
   };
 </script>
