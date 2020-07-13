33808f533d85bf33b273ddd222c7ee8c727ab540
jinyanwei <jinyanwei@onetarget.cn>
2020-07-13 14:11:07 +0800

    slider 离散值

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,19 @@
 <template>
   <div id="app">
     <div class="block">
-      <span class="demostration">默认</span>
-      <el-slider v-model="value1"></el-slider>
+      <span class="demostration">不显示间隔点</span>
+      <el-slider
+        v-model="value1"
+        :step="10"
+      ></el-slider>
     </div>
     <div class="block">
-      <span class="demostration">自定义初始值</span>
-      <el-slider v-model="value2"></el-slider>
-    </div>
-    <div class="block">
-      <span class="demostration">隐藏 Tooltip</span>
-      <el-slider v-model="value3" :show-tooltip="false"></el-slider>
-    </div>
-    <div class="block">
-      <span class="demostration">格式化 Tooltip</span>
-      <el-slider v-model="value4" :format-tooltip="formatTooltip"></el-slider>
-    </div>
-    <div class="block">
-      <span class="demostration">禁用</span>
-      <el-slider v-model="value5" disabled></el-slider>
+      <span class="demostration">显示间隔点</span>
+      <el-slider
+        v-model="value2"
+        :step="10"
+        show-stops
+      ></el-slider>
     </div>
   </div>
 </template>
@@ -30,16 +25,10 @@
     data() {
       return {
         value1: 0,
-        value2: 50,
-        value3: 36,
-        value4: 48,
-        value5: 42
+        value2: 0,
       }
     },
     methods: {
-      formatTooltip(value) {
-        return value / 100;
-      }
     }
   };
 </script>
