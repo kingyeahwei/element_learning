a65a2a37a9d7b7d72a62fcdccc8cb3489abbe7a6
jinyanwei <jinyanwei@live.com>
2020-07-13 23:35:00 +0800

    datetimepicker 默认的起始与结束时刻

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,24 @@
 <template>
   <div id="app">
     <div class="block">
-      <span>默认</span>
+      <span>起始日期时刻为 12:00:00</span>
       <el-date-picker
         v-model="value1"
         type="datetimerange"
-        range-separator="至"
         start-placeholder="开始日期"
         end-placeholder="结束日期"
+        :default-time="['12:00:00']"
       ></el-date-picker>
     </div>
     <div class="block">
-      <span>带快捷选项</span>
+      <span>起始日期时刻为 12:00:00 结束日期时刻为 08:00:00</span>
       <el-date-picker
         v-model="value2"
         type="datetimerange"
-        :picker-options="pickerOptions"
-        range-separator="至"
+        align="right"
         start-placeholder="开始日期"
         end-placeholder="结束日期"
-        align="right"
+        :default-time="['12:00:00', '08:00:00']"
       ></el-date-picker>
     </div>
   </div>
@@ -31,38 +30,7 @@
     name: "app",
     data() {
       return {
-        pickerOptions: {
-          shortcuts: [
-            {
-              text: "最近一周",
-              onClick(picker) {
-                const end = new Date();
-                const start = new Date();
-                start.setTime(start.getTime() - 3600 * 24 * 1000 * 7)
-                picker.$emit("pick", [start, end])
-              }
-            },
-            {
-              text: "最近一个月",
-              onClick(picker) {
-                const end = new Date();
-                const start = new Date();
-                start.setTime(start.getTime() - 3600 * 1000 * 24 * 30);
-                picker.$emit("pick", [start, end])
-              }
-            },
-            {
-              text: "最近三个月",
-              onClick(picker) {
-                const end = new Date();
-                const start = new Date();
-                start.setTime(start.getTime() - 3600 * 1000 * 24 * 90)
-                picker.$emit('pick', [start, end])
-              }
-            }
-          ]
-        },
-        value1: [new Date(2000, 10, 10, 10, 10), new Date(2000, 10, 11, 10, 10)],
+        value1: "",
         value2: ""
       };
     },
