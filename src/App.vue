61e996c9ad8590bed4ecceff428897493b7b85e2
jinyanwei <jinyanwei@onetarget.cn>
2020-07-13 11:11:15 +0800

    select 网络搜索

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,15 @@
 <template>
   <div id="app">
-    <el-select v-model="value" filterable placeholder="请选择">
+    <el-select
+      v-model="value"
+      multiple
+      filterable
+      remote
+      reserve-keyword
+      collapse-tags
+      placeholder="请输入关键字"
+      :remote-method="remoteMethod"
+      :loading="loading">
       <el-option
         v-for="item in options"
         :key="item.value"
@@ -16,28 +25,49 @@
     name: "app",
     data() {
       return {
-        options: [
-          {
-            value: '选项1',
-            label: '黄金糕'
-          }, {
-            value: '选项2',
-            label: '双皮奶'
-          }, {
-            value: '选项3',
-            label: '蚵仔煎'
-          }, {
-            value: '选项4',
-            label: '龙须面'
-          }, {
-            value: '选项5',
-            label: '北京烤鸭'
-          }
-        ],
-        value: ''
+        options: [],
+        value: [],
+        list: [],
+        loading: false,
+        states: ["Alabama", "Alaska", "Arizona",
+          "Arkansas", "California", "Colorado",
+          "Connecticut", "Delaware", "Florida",
+          "Georgia", "Hawaii", "Idaho", "Illinois",
+          "Indiana", "Iowa", "Kansas", "Kentucky",
+          "Louisiana", "Maine", "Maryland",
+          "Massachusetts", "Michigan", "Minnesota",
+          "Mississippi", "Missouri", "Montana",
+          "Nebraska", "Nevada", "New Hampshire",
+          "New Jersey", "New Mexico", "New York",
+          "North Carolina", "North Dakota", "Ohio",
+          "Oklahoma", "Oregon", "Pennsylvania",
+          "Rhode Island", "South Carolina",
+          "South Dakota", "Tennessee", "Texas",
+          "Utah", "Vermont", "Virginia",
+          "Washington", "West Virginia", "Wisconsin",
+          "Wyoming"]
       };
     },
-    methods: {}
+    mounted() {
+      this.list = this.states.map(item => {
+        return {value: `value:${item}`, label: `label:${item}`}
+      })
+    },
+    methods: {
+      remoteMethod(query) {
+        if(query != "") {
+          this.loading = true;
+          setTimeout(() => {
+            this.loading = false;
+            this.options = this.list.filter((item) => {
+              return item.label.toLowerCase().indexOf(query.toLowerCase()) > -1;
+            })
+          }, 200)
+        } else {
+          this.options = [];
+        }
+      }
+    }
   };
 </script>
 
