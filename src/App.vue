9a5c0c8d2dd2833e3f93e2b6e5ac584d43611a71
jinyanwei <jinyanwei@live.com>
2020-07-14 00:36:17 +0800

    transfer 基础用法

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
-    <el-color-picker v-model="color"></el-color-picker>
-    <el-color-picker v-model="color" size="medium"></el-color-picker>
-    <el-color-picker v-model="color" size="small"></el-color-picker>
-    <el-color-picker v-model="color" size="mini"></el-color-picker>
+    <el-transfer
+      v-model="value"
+      :data="data"
+    ></el-transfer>
   </div>
 </template>
 
@@ -12,8 +12,20 @@
   export default {
     name: "app",
     data() {
+      const generateData = (_) => {
+        const data = [];
+        for (let i = 1; i <= 15; i++) {
+          data.push({
+            key: i,
+            label: `备选项${i}`,
+            disable: i % 4 == 0
+          })
+        }
+        return data
+      }
       return {
-        color: '#409eff',
+        data: generateData(),
+        value: [1, 4]
       };
     },
     methods: {}
