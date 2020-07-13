ef025f76b5d762be27679f384ae29cd5195f7215
jinyanwei <jinyanwei@onetarget.cn>
2020-07-13 11:49:14 +0800

    cascader 禁用状态

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,6 @@
 <template>
   <div id="app">
-    <div class="block">
-      <span class="demoNstration">默认click触发子菜单</span>
-      <el-cascader
-        v-model="value"
-        :options="options"
-        @change="handleChange"
-      ></el-cascader>
-    </div>
-    <div class="block">
-      <span class="demoNstration">hover 触发子菜单</span>
-      <el-cascader
-        v-model="value"
-        :options="options"
-        :props="{expandTrigger: 'hover'}"
-        @change="handleChange"
-      ></el-cascader>
-    </div>
+    <el-cascader :options="options"></el-cascader>
   </div>
 </template>
 
@@ -29,6 +13,7 @@
           {
             value: 'zhinan',
             label: '指南',
+            disabled: true,
             children: [
               {
                 value: 'shejiyuanze',
@@ -65,7 +50,8 @@
                     label: '顶部导航'
                   }
                 ]
-              }]
+              }
+            ]
           },
           {
             value: 'zujian',
@@ -96,7 +82,8 @@
                     label: 'Button 按钮'
                   }
                 ]
-              }, {
+              },
+              {
                 value: 'form',
                 label: 'Form',
                 children: [
@@ -157,115 +144,90 @@
                     label: 'Form 表单'
                   }
                 ]
-              }, {
+              },
+              {
                 value: 'data',
                 label: 'Data',
-                children: [
-                  {
-                    value: 'table',
-                    label: 'Table 表格'
-                  },
-                  {
-                    value: 'tag',
-                    label: 'Tag 标签'
-                  },
-                  {
-                    value: 'progress',
-                    label: 'Progress 进度条'
-                  },
-                  {
-                    value: 'tree',
-                    label: 'Tree 树形控件'
-                  },
-                  {
-                    value: 'pagination',
-                    label: 'Pagination 分页'
-                  },
-                  {
-                    value: 'badge',
-                    label: 'Badge 标记'
-                  }
-                ]
+                children: [{
+                  value: 'table',
+                  label: 'Table 表格'
+                }, {
+                  value: 'tag',
+                  label: 'Tag 标签'
+                }, {
+                  value: 'progress',
+                  label: 'Progress 进度条'
+                }, {
+                  value: 'tree',
+                  label: 'Tree 树形控件'
+                }, {
+                  value: 'pagination',
+                  label: 'Pagination 分页'
+                }, {
+                  value: 'badge',
+                  label: 'Badge 标记'
+                }]
               }, {
                 value: 'notice',
                 label: 'Notice',
-                children: [
-                  {
-                    value: 'alert',
-                    label: 'Alert 警告'
-                  },
-                  {
-                    value: 'loading',
-                    label: 'Loading 加载'
-                  },
-                  {
-                    value: 'message',
-                    label: 'Message 消息提示'
-                  },
-                  {
-                    value: 'message-box',
-                    label: 'MessageBox 弹框'
-                  },
-                  {
-                    value: 'notification',
-                    label: 'Notification 通知'
-                  }]
+                children: [{
+                  value: 'alert',
+                  label: 'Alert 警告'
+                }, {
+                  value: 'loading',
+                  label: 'Loading 加载'
+                }, {
+                  value: 'message',
+                  label: 'Message 消息提示'
+                }, {
+                  value: 'message-box',
+                  label: 'MessageBox 弹框'
+                }, {
+                  value: 'notification',
+                  label: 'Notification 通知'
+                }]
               }, {
                 value: 'navigation',
                 label: 'Navigation',
-                children: [
-                  {
-                    value: 'menu',
-                    label: 'NavMenu 导航菜单'
-                  },
-                  {
-                    value: 'tabs',
-                    label: 'Tabs 标签页'
-                  },
-                  {
-                    value: 'breadcrumb',
-                    label: 'Breadcrumb 面包屑'
-                  },
-                  {
-                    value: 'dropdown',
-                    label: 'Dropdown 下拉菜单'
-                  },
-                  {
-                    value: 'steps',
-                    label: 'Steps 步骤条'
-                  }
-                ]
+                children: [{
+                  value: 'menu',
+                  label: 'NavMenu 导航菜单'
+                }, {
+                  value: 'tabs',
+                  label: 'Tabs 标签页'
+                }, {
+                  value: 'breadcrumb',
+                  label: 'Breadcrumb 面包屑'
+                }, {
+                  value: 'dropdown',
+                  label: 'Dropdown 下拉菜单'
+                }, {
+                  value: 'steps',
+                  label: 'Steps 步骤条'
+                }]
               }, {
                 value: 'others',
                 label: 'Others',
-                children: [
-                  {
-                    value: 'dialog',
-                    label: 'Dialog 对话框'
-                  },
-                  {
-                    value: 'tooltip',
-                    label: 'Tooltip 文字提示'
-                  },
-                  {
-                    value: 'popover',
-                    label: 'Popover 弹出框'
-                  },
-                  {
-                    value: 'card',
-                    label: 'Card 卡片'
-                  },
-                  {
-                    value: 'carousel',
-                    label: 'Carousel 走马灯'
-                  },
-                  {
-                    value: 'collapse',
-                    label: 'Collapse 折叠面板'
-                  }
-                ]
-              }
-            ]
+                children: [{
+                  value: 'dialog',
+                  label: 'Dialog 对话框'
+                }, {
+                  value: 'tooltip',
+                  label: 'Tooltip 文字提示'
+                }, {
+                  value: 'popover',
+                  label: 'Popover 弹出框'
+                }, {
+                  value: 'card',
+                  label: 'Card 卡片'
+                }, {
+                  value: 'carousel',
+                  label: 'Carousel 走马灯'
+                }, {
+                  value: 'collapse',
+                  label: 'Collapse 折叠面板'
+                }]
+              }]
           },
           {
             value: 'ziyuan',
@@ -285,10 +247,9 @@
               }
             ]
           }
-        ],
-        value: []
-      };
-    },
+        ]
+      }
+    }
   };
 </script>
 
