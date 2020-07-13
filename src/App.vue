2da324c50403bf8f2493a663e25b545362525e1a
jinyanwei <jinyanwei@onetarget.cn>
2020-07-13 11:19:04 +0800

    select 可以创建条目

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,9 @@
       v-model="value"
       multiple
       filterable
-      remote
-      reserve-keyword
-      collapse-tags
-      placeholder="请输入关键字"
-      :remote-method="remoteMethod"
-      :loading="loading">
+      allow-create
+      default-first-option
+      placeholder="请选择文章标签">
       <el-option
         v-for="item in options"
         :key="item.value"
@@ -25,49 +22,21 @@
     name: "app",
     data() {
       return {
-        options: [],
-        value: [],
-        list: [],
-        loading: false,
-        states: ["Alabama", "Alaska", "Arizona",
-          "Arkansas", "California", "Colorado",
-          "Connecticut", "Delaware", "Florida",
-          "Georgia", "Hawaii", "Idaho", "Illinois",
-          "Indiana", "Iowa", "Kansas", "Kentucky",
-          "Louisiana", "Maine", "Maryland",
-          "Massachusetts", "Michigan", "Minnesota",
-          "Mississippi", "Missouri", "Montana",
-          "Nebraska", "Nevada", "New Hampshire",
-          "New Jersey", "New Mexico", "New York",
-          "North Carolina", "North Dakota", "Ohio",
-          "Oklahoma", "Oregon", "Pennsylvania",
-          "Rhode Island", "South Carolina",
-          "South Dakota", "Tennessee", "Texas",
-          "Utah", "Vermont", "Virginia",
-          "Washington", "West Virginia", "Wisconsin",
-          "Wyoming"]
+        options: [
+          {
+            value: 'HTML',
+            label: 'HTML'
+          }, {
+            value: 'CSS',
+            label: 'CSS'
+          }, {
+            value: 'JavaScript',
+            label: 'JavaScript'
+          }
+        ],
+        value: []
       };
     },
-    mounted() {
-      this.list = this.states.map(item => {
-        return {value: `value:${item}`, label: `label:${item}`}
-      })
-    },
-    methods: {
-      remoteMethod(query) {
-        if(query != "") {
-          this.loading = true;
-          setTimeout(() => {
-            this.loading = false;
-            this.options = this.list.filter((item) => {
-              return item.label.toLowerCase().indexOf(query.toLowerCase()) > -1;
-            })
-          }, 200)
-        } else {
-          this.options = [];
-        }
-      }
-    }
   };
 </script>
 
