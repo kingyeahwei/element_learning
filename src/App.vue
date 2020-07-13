e1662af98e40c6c3ecd9156c3923f2c750ed00ac
jinyanwei <jinyanwei@onetarget.cn>
2020-07-13 14:12:31 +0800

    slider 带有输入框

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,9 @@
 <template>
   <div id="app">
     <div class="block">
-      <span class="demostration">不显示间隔点</span>
       <el-slider
-        v-model="value1"
-        :step="10"
-      ></el-slider>
-    </div>
-    <div class="block">
-      <span class="demostration">显示间隔点</span>
-      <el-slider
-        v-model="value2"
-        :step="10"
-        show-stops
+        v-model="value"
+        show-input
       ></el-slider>
     </div>
   </div>
@@ -24,8 +15,7 @@
     name: "app",
     data() {
       return {
-        value1: 0,
-        value2: 0,
+        value: 0,
       }
     },
     methods: {
