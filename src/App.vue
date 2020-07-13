d783f62ff178cc1c0c64ded626b86271f79ac03b
jinyanwei <jinyanwei@live.com>
2020-07-13 23:27:45 +0800

    datetimepicker 日期和时间范围

--- src/App.vue
+++ src/App.vue
@@ -4,27 +4,22 @@
       <span>默认</span>
       <el-date-picker
         v-model="value1"
-        type="datetime"
-        placeholder="选择日期时间"
+        type="datetimerange"
+        range-separator="至"
+        start-placeholder="开始日期"
+        end-placeholder="结束日期"
       ></el-date-picker>
     </div>
     <div class="block">
       <span>带快捷选项</span>
       <el-date-picker
         v-model="value2"
-        type="datetime"
-        placeholder="选择日期时间"
-        align="right"
+        type="datetimerange"
         :picker-options="pickerOptions"
-      ></el-date-picker>
-    </div>
-    <div class="block">
-      <span>设置默认时间</span>
-      <el-date-picker
-        v-model="value3"
-        type="datetime"
-        placeholder="选择日期时间"
-        default-time="12:00:00"
+        range-separator="至"
+        start-placeholder="开始日期"
+        end-placeholder="结束日期"
+        align="right"
       ></el-date-picker>
     </div>
   </div>
@@ -39,32 +34,36 @@
         pickerOptions: {
           shortcuts: [
             {
-              text: "今天",
+              text: "最近一周",
               onClick(picker) {
-                picker.$emit("pick", new Date())
+                const end = new Date();
+                const start = new Date();
+                start.setTime(start.getTime() - 3600 * 24 * 1000 * 7)
+                picker.$emit("pick", [start, end])
               }
             },
             {
-              text: "昨天",
+              text: "最近一个月",
               onClick(picker) {
-                const date = new Date();
-                date.setTime(date.getTime() - 3600 * 1000 * 24)
-                picker.$emit("pick", date)
+                const end = new Date();
+                const start = new Date();
+                start.setTime(start.getTime() - 3600 * 1000 * 24 * 30);
+                picker.$emit("pick", [start, end])
               }
             },
             {
-              text: "一周前",
+              text: "最近三个月",
               onClick(picker) {
-                const date = new Date();
-                date.setTime(date.getTime() - 3600 * 1000 * 24 * 7)
-                picker.$emit("pick", date)
+                const end = new Date();
+                const start = new Date();
+                start.setTime(start.getTime() - 3600 * 1000 * 24 * 90)
+                picker.$emit('pick', [start, end])
               }
             }
           ]
         },
-        value1: "",
-        value2: "",
-        value3: "",
+        value1: [new Date(2000, 10, 10, 10, 10), new Date(2000, 10, 11, 10, 10)],
+        value2: ""
       };
     },
     methods: {}
