74797a9f5ee845b5c224443ffaa8ca4395da5e51
jinyanwei <jinyanwei@live.com>
2020-07-13 23:46:27 +0800

    rate 辅助文字

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,6 @@
 <template>
   <div id="app">
-    <div class="block">
-      <span>默认不区分颜色</span>
-      <el-rate v-model="value1"></el-rate>
-    </div>
-    <div class="block">
-      <span>区分颜色</span>
-      <el-rate v-model="value2" :colors="colors"></el-rate>
-    </div>
+    <el-rate v-model="value" show-text :texts="texts"></el-rate>
   </div>
 </template>
 
@@ -17,9 +10,8 @@
     name: "app",
     data() {
       return {
-        value1: null,
-        value2: null,
-        colors: ["#99a9bf", "#f7ba2a", "#ff9900"]
+        value: "",
+        texts: ["1", "2", "3", "4", "5"]
       };
     },
     methods: {}
