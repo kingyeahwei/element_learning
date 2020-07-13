01c1f3142045748e091b09e04c10b8de427de947
jinyanwei <jinyanwei@live.com>
2020-07-14 00:22:29 +0800

    colorpicker 定义大小

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
-    <el-color-picker
-      show-alpha
-      :predefine="predefineColors"
-    ></el-color-picker>
+    <el-color-picker v-model="color"></el-color-picker>
+    <el-color-picker v-model="color" size="medium"></el-color-picker>
+    <el-color-picker v-model="color" size="small"></el-color-picker>
+    <el-color-picker v-model="color" size="mini"></el-color-picker>
   </div>
 </template>
 
@@ -13,23 +13,7 @@
     name: "app",
     data() {
       return {
-        color: 'rgba(255, 69, 0, 0.68)',
-        predefineColors: [
-          '#ff4500',
-          '#ff8c00',
-          '#ffd700',
-          '#90ee90',
-          '#00ced1',
-          '#1e90ff',
-          '#c71585',
-          'rgba(255, 69, 0, 0.68)',
-          'rgb(255, 120, 0)',
-          'hsv(51, 100, 98)',
-          'hsva(120, 40, 94, 0.5)',
-          'hsl(181, 100%, 37%)',
-          'hsla(209, 100%, 56%, 0.73)',
-          '#c7158577'
-        ]
+        color: '#409eff',
       };
     },
     methods: {}
