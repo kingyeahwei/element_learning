adf5163a65e5fed9344ec7119b65393fc49a0f49
jinyanwei <jinyanwei@onetarget.cn>
2020-07-13 10:28:47 +0800

    冲突解决

--- src/App.vue
+++ src/App.vue
@@ -11,47 +11,10 @@
 <script>
   export default {
     name: "app",
-    data: function () {
-      return {
-        options: [
-          {
-            label: '热门城市',
-            options: [
-              {
-                value: 'Shanghai',
-                label: '上海'
-              },
-              {
-                value: 'Beijing',
-                label: '北京'
-              }
-            ]
-          },
-          {
-            label: '城市名',
-            options: [
-              {
-                value: 'Chengdu',
-                label: '成都'
-              }, {
-                value: 'Shenzhen',
-                label: '深圳'
-              }, {
-                value: 'Guangzhou',
-                label: '广州'
-              }, {
-                value: 'Dalian',
-                label: '大连'
-              }
-            ]
-          }
-        ],
-        value: "",
-      };
+    data() {
+      return {};
     },
-    methods: {},
-    mounted() {
-    }
+    methods: {}
   };
 </script>
 
