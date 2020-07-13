8eb729fcca984dcf2efd69937b8af48d7befa667
jinyanwei <jinyanwei@onetarget.cn>
2020-07-13 13:53:24 +0800

    switch 拓展的value类型

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <template>
   <div id="app">
-    <el-switch v-model="value1" active-text="按月付费" inactive-text="按年付费"></el-switch>
-    <el-switch
-      v-model="value2"
-      active-color="#13ce66"
-      inactive-color="#ff4949"
-      active-text="按月付费"
-      inactive-text="按年付费"
-    ></el-switch>
+    <el-tooltip :content="'Switch value' + value" placement="top">
+      <el-switch
+        v-model="value"
+        active-color="#13ce66"
+        inactive-color="#ff4949"
+        active-value="100"
+        inactive-value="0"
+      ></el-switch>
+    </el-tooltip>
   </div>
 </template>
 
@@ -17,8 +18,7 @@
     name: "app",
     data() {
       return {
-        value1: true,
-        value2: true
+        value: "100"
       }
     }
   };
