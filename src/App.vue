6e0bd60f9a9fb227b754fda347a31e5a2073333b
jinyanwei <jinyanwei@onetarget.cn>
2020-07-13 14:48:00 +0800

    timepicker 任意时间范围

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,22 @@
 <template>
   <div id="app">
-    <el-time-select
-      placeholder="起始时间"
-      v-model="startTime"
-      :picker-options="{
-        start: '08:30',
-        step: '00:15',
-        end: '18:30',
-        maxTime: endTime
-      }"
-    ></el-time-select>
-    <el-time-select
-      placeholder="结束时间"
-      v-model="endTime"
-      :picker-options="{
-        start: '08:30',
-        step: '00:15',
-        end: '18:30',
-        minTime: startTime
-      }"
-    ></el-time-select>
+    <el-time-picker
+      is-range
+      v-model="value1"
+      range-separator="至"
+      start-placeholder="开始时间"
+      end-placeholder="结束时间"
+      placeholder="选择时间范围"
+    ></el-time-picker>
+    <el-time-picker
+      is-range
+      arrow-control
+      v-model="value2"
+      range-separator="至"
+      start-placeholder="开始时间"
+      end-placeholder="结束时间"
+      placeholder="选择时间范围"
+    ></el-time-picker>
   </div>
 </template>
 
@@ -29,8 +26,8 @@
     name: "app",
     data() {
       return {
-        startTime: "",
-        endTime: ""
+        value1: [new Date(2016, 9, 10, 8, 40), new Date(2016, 9, 10, 9, 40)],
+        value2: [new Date(2016, 9, 10, 8, 40), new Date(2016, 9, 10, 9, 40)]
       }
     },
     methods: {
