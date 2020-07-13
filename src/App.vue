c6d24b9958a85527f8bb4c16591beec9b99037d5
jinyanwei <jinyanwei@onetarget.cn>
2020-07-13 10:50:35 +0800

    select 可搜索

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
   <div id="app">
-    <el-select v-model="value" placeholder="请选择">
-      <el-option-group v-for="group in options" :key="group.label" :label="group.label">
-        <el-option v-for="item in group.options" :key="item.value" :label="item.label" :value="item.value"></el-option>
-      </el-option-group>
+    <el-select v-model="value" filterable placeholder="请选择">
+      <el-option
+        v-for="item in options"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value"
+      ></el-option>
     </el-select>
   </div>
 </template>
@@ -12,7 +15,27 @@
   export default {
     name: "app",
     data() {
-      return {};
+      return {
+        options: [
+          {
+            value: '选项1',
+            label: '黄金糕'
+          }, {
+            value: '选项2',
+            label: '双皮奶'
+          }, {
+            value: '选项3',
+            label: '蚵仔煎'
+          }, {
+            value: '选项4',
+            label: '龙须面'
+          }, {
+            value: '选项5',
+            label: '北京烤鸭'
+          }
+        ],
+        value: ''
+      };
     },
     methods: {}
   };
