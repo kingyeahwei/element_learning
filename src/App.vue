5a7b0a00aba3e0ee360bcf1df821cc7067b5742a
jinyanwei <jinyanwei@live.com>
2020-07-14 22:58:15 +0800

    transfer 数据项属性别名

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,10 @@
 <template>
   <div id="app">
-    <p style="text-align: center; margin: 0 0 20px;">使用render-content自定义数据项</p>
-    <div style="text-align: center;">
-      <el-transfer
-        style="text-align: left; display: inline-block;"
-        v-model="value"
-        filterable
-        :left-default-checked="[2, 3]"
-        :right-default-checked="[1]"
-        :render-content="renderFunc"
-        :titles="['到左边', '到右边']"
-        :button-texts="['向左', '向右']"
-        :format="{ noChecked: '${total}', hasChecked: '${checked}/${total}' }"
-        @change="handleChange"
-        :data="data">
-        <el-button class="transfer-footer" slot="left-footer" size="small">操作</el-button>
-        <el-button class="transfer-footer" slot="right-footer" size="small">操作</el-button>
-      ></el-transfer>
-    </div>
-    <p style="text-align: center; margin: 50px 0 20px;">使用scoped-slot自定义数据项</p>
-    <div style="text-align: center;">
-      <el-transfer
-        style="text-align: left; display: inline-block;"
-        v-model="value4"
-        filterable
-        :left-default-checked="[2, 3]"
-        :right-default-checked="[1]"
-        :titles="['Source', 'Target']"
-        :button-texts="['到左边', '到右边']"
-        :format="{ noChecked: '${total}', hasChecked: '${checked}/${total}' }"
-        @change="handleChange"
-        :data="data">
-        <span slot-scope="{option}">{{option.key}}++{{option.label}}</span>
-        <el-button class="transfer-footer" slot="left-footer" size="small">操作</el-button>
-        <el-button class="transfer-footer" slot="right-footer" size="small">操作</el-button>
-      </el-transfer>
-    </div>
+    <el-transfer
+      v-model="value"
+      :props="{key: 'value', label: 'desc'}"
+      :data="data"
+    ></el-transfer>
   </div>
 </template>
 
@@ -44,39 +13,26 @@
   export default {
     name: "app",
     data() {
-      const generateData = (_) => {
+      const generateData = () => {
         const data = [];
         for (let i = 1; i <= 15; i++) {
           data.push({
-            key: i,
-            label: `备选项${i}`,
-            disabled: i % 4 == 0
+            value: i,
+            desc: `备选项${i}`,
+            disabled: i % 4 === 0
           })
         }
         return data;
       }
       return {
         data: generateData(),
-        value: [1],
-        value4: [1],
-        renderFunc(h, option) {
-          console.log(h)
-          console.log(option)
-          return <span>{option.key} ---- {option.label}</span>;
-        }
+        value: []
       }
     },
     methods: {
-      handleChange(value, direction, movedkeys) {
-        console.log(value, direction, movedkeys)
-      }
     }
   };
 </script>
 
 <style lang="less">
-  .transfer-footer {
-    margin-left: 20px;
-    padding: 6px 5px;
-  }
 </style>
