bd24aebd7a07c23a57436e52e23d1682cd0f9821
jinyanwei <jinyanwei@live.com>
2020-07-14 23:27:07 +0800

    form 行内表单

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,17 @@
 <template xmlns="http://www.w3.org/1999/html">
   <div id="app">
-    <el-form ref="form" :model="form" label-width="80px">
-      <el-form-item label="活动名称">
-        <el-input v-model="form.name"></el-input>
+    <el-form :model="formInline" class="demo-form-inline" :inline="true">
+      <el-form-item label="审批人">
+        <el-input v-model="formInline.user" placeholder="审批人"></el-input>
       </el-form-item>
       <el-form-item label="活动区域">
-        <el-select v-model="form.region" placeholder="请选择活动区域">
+        <el-select v-model="formInline.region" placeholder="活动区域">
           <el-option label="区域1" value="shanghai"></el-option>
           <el-option label="区域2" value="beijing"></el-option>
         </el-select>
       </el-form-item>
-      <el-form-item label="活动时间">
-        <el-col :span="11">
-          <el-date-picker type="date" placeholder="选址日期" v-model="form.date1" style="width: 100%;"></el-date-picker>
-        </el-col>
-        <el-col :span="2">-</el-col>
-        <el-col :span="11">
-          <el-time-picker placeholder="选择时间" v-model="form.date2" style="width: 100%"></el-time-picker>
-        </el-col>
-      </el-form-item>
-      <el-form-item label="即时配送">
-        <el-switch v-model="form.delivery"></el-switch>
-      </el-form-item>
-      <el-form-item label="活动性质">
-        <el-checkbox-group v-model="form.type">
-          <el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
-          <el-checkbox label="地推活动" name="type"></el-checkbox>
-          <el-checkbox label="线下主题活动" name="type"></el-checkbox>
-          <el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
-        </el-checkbox-group>
-      </el-form-item>
-      <el-form-item label="特殊资源">
-        <el-radio-group v-model="form.resource">
-          <el-radio label="线上品牌商赞助"></el-radio>
-          <el-radio label="线下场地免费"></el-radio>
-        </el-radio-group>
-      </el-form-item>
-      <el-form-item label="活动形式">
-        <el-input type="textarea" v-model="form.desc"></el-input>
-      </el-form-item>
       <el-form-item>
-        <el-button type="primary" @click="onsubmit">立即创建</el-button>
-        <el-button>取消</el-button>
+        <el-button type="primary" @click="onSubmit">查询</el-button>
       </el-form-item>
     </el-form>
   </div>
@@ -53,19 +23,16 @@
     name: "app",
     data() {
       return {
-        form: {
-          name: "",
-          region: "",
-          date1: "",
-          date2: "",
-          delivery: false,
-          type: [],
-          resource: "",
-          desc: ""
+        formInline: {
+          user: "",
+          region: ""
         }
       }
     },
     methods: {
+      onSubmit() {
+        console.log("submit!!")
+      }
     }
   };
 </script>
