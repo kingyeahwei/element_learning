ff5ce29788e0a3aad80f47c2540cd320ca17666f
jinyanwei <jinyanwei@onetarget.cn>
2020-07-15 13:22:08 +0800

    table 带边框表格

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,24 @@
 <template>
   <div id="app">
-    <el-form ref="form" :model="sizeForm" label-width="80px" size="mini">
-      <el-form-item label="活动名称">
-        <el-input v-model="sizeForm.name"></el-input>
-      </el-form-item>
-      <el-form-item label="活动区域">
-        <el-select v-model="sizeForm.region" placeholder="请选择活动区域">
-          <el-option label="区域1" value="shanghai"></el-option>
-          <el-option label="区域2" value="beijing"></el-option>
-        </el-select>
-      </el-form-item>
-      <el-form-item label="活动时间">
-        <el-col :span="11">
-          <el-date-picker type="date" placeholder="选择日期" v-model="sizeForm.date1" style="width: 100%"></el-date-picker>
-        </el-col>
-        <el-col :span="2" class="line" style="text-align: center">-</el-col>
-        <el-col :span="11">
-          <el-time-picker placeholder="选择时间" v-model="sizeForm.date2" style="width: 100%"></el-time-picker>
-        </el-col>
-      </el-form-item>
-      <el-form-item label="活动性质">
-        <el-checkbox-group v-model="sizeForm.type">
-          <el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
-          <el-checkbox label="地推活动" name="type"></el-checkbox>
-          <el-checkbox label="线下主题活动" name="type"></el-checkbox>
-        </el-checkbox-group>
-      </el-form-item>
-      <el-form-item label="特殊资源">
-        <el-radio-group v-model="sizeForm.resource">
-          <el-radio border label="线上品牌商赞助"></el-radio>
-          <el-radio border label="线下场地免费"></el-radio>
-        </el-radio-group>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="onSubmit">立即创建</el-button>
-        <el-button>取消</el-button>
-      </el-form-item>
-    </el-form>
+    <el-table
+      :data="tableData"
+      border
+      style="width: 100%">
+      <el-table-column
+        prop="date"
+        label="日期"
+        width="180"
+      ></el-table-column>
+      <el-table-column
+        prop="name"
+        label="姓名"
+        width="180"
+      ></el-table-column>
+      <el-table-column
+        prop="address"
+        label="地址"
+      ></el-table-column>
+    </el-table>
   </div>
 </template>
 
@@ -46,23 +28,28 @@
         name: "app",
         data() {
             return {
-                sizeForm: {
-                    name:"",
-                    region: "",
-                    date1: "",
-                    date2: "",
-                    delivery: "",
-                    type: [],
-                    resource: "",
-                    desc: ""
-                }
-            };
-        },
-        methods: {
-            onSubmit() {
-                console.log("submit!!!")
+                tableData: [
+                    {
+                        date: '2016-05-02',
+                        name: '王小虎',
+                        address: '上海市普陀区金沙江路 1518 弄'
+                    }, {
+                        date: '2016-05-04',
+                        name: '王小虎',
+                        address: '上海市普陀区金沙江路 1517 弄'
+                    }, {
+                        date: '2016-05-01',
+                        name: '王小虎',
+                        address: '上海市普陀区金沙江路 1519 弄'
+                    }, {
+                        date: '2016-05-03',
+                        name: '王小虎',
+                        address: '上海市普陀区金沙江路 1516 弄'
+                    }
+                ]
             }
-        }
+        },
+        methods: {}
     };
 </script>
 
