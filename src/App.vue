f4a6deac4a1bfec2f05e4618be5a064eba924da2
jinyanwei <jinyanwei@onetarget.cn>
2020-07-15 18:28:55 +0800

    tree 可选择动态加载节点

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="app">
-    <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+    <el-tree
+      :props="props"
+      :load="loadNode"
+      lazy
+      show-checkbox
+      @check-change="handleCheckChange">
+    </el-tree>
   </div>
 </template>
 
@@ -10,52 +16,50 @@
         name: "app",
         data() {
             return {
-                data: [
-                    {
-                        label: '一级 1',
-                        children: [{
-                            label: '二级 1-1',
-                            children: [{
-                                label: '三级 1-1-1'
-                            }]
-                        }]
-                    }, {
-                        label: '一级 2',
-                        children: [{
-                            label: '二级 2-1',
-                            children: [{
-                                label: '三级 2-1-1'
-                            }]
-                        }, {
-                            label: '二级 2-2',
-                            children: [{
-                                label: '三级 2-2-1'
-                            }]
-                        }]
-                    }, {
-                        label: '一级 3',
-                        children: [{
-                            label: '二级 3-1',
-                            children: [{
-                                label: '三级 3-1-1'
-                            }]
-                        }, {
-                            label: '二级 3-2',
-                            children: [{
-                                label: '三级 3-2-1'
-                            }]
-                        }]
-                    }
-                ],
-                defaultProps: {
-                    children: "children",
-                    label: "label"
-                }
+                props: {
+                    label: "name",
+                    children: "zones"
+                },
+                count: 1
             }
         },
         methods: {
+            handleCheckChange(data, checked, indeterminate) {
+                console.log(data, checked, indeterminate)
+            },
             handleNodeClick(data) {
                 console.log(data)
+            },
+            loadNode(node, resolve) {
+                if (node.level == 0) {
+                    return resolve([
+                        {name: "region1"},
+                        {name: "region2"}
+                    ])
+                }
+                if (node.level > 3) {
+                    return resolve([]);
+                }
+                var hasChild;
+                if (node.data.name == "region1") {
+                    hasChild = true;
+                } else if (node.data.name == "region2") {
+                    hasChild = false;
+                } else {
+                    hasChild = Math.random() > 0.5;
+                }
+                setTimeout(() => {
+                    var data;
+                    if (hasChild) {
+                        data = [
+                            {name: "zone" + this.count++},
+                            {name: "zone" + this.count++}
+                        ];
+                    } else  {
+                        data = [];
+                    }
+                    resolve(data)
+                }, 500);
             }
         }
     };
