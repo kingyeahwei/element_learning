6a9c69445cecbd208f5fddec5805a8a3a181e8c4
jinyanwei <jinyanwei@live.com>
2020-07-15 22:33:54 +0800

    tree 节点过滤

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,15 @@
 <template>
   <div id="app">
-    <div class="block">
-      <p>使用 render-content</p>
-      <el-tree
-        :data="data1"
-        show-checkbox
-        node-key="id"
-        default-expand-all
-        :expand-on-click-node="false"
-        :render-content="renderContent"
-      ></el-tree>
-    </div>
-    <div class="block">
-      <p>使用 scoped slot</p>
-      <el-tree
-        :data="data2"
-        show-checkbox
-        node-key="id"
-        default-expand-all
-        :expand-on-click-node="false">
-        <span class="custom-tree-node" slot-scope="{node, data}">
-          <span>{{node.label}}</span>
-          <span>
-            <el-button
-              type="text"
-              size="mini"
-              @click="() => {append(data)}"
-            >Append</el-button>
-            <el-button
-              type="text"
-              size="mini"
-              @click="() => {remove(node, data)}"
-            >Delete</el-button>
-          </span>
-        </span>
-      </el-tree>
-    </div>
+    <el-input v-model="filterText" placeholder="输入关键字过滤"></el-input>
+    <el-tree
+      class="filter-tree"
+      :data="data"
+      :props="defaultProps"
+      default-expand-all
+      :filter-node-method="filterNode"
+      ref="tree"
+    ></el-tree>
+
   </div>
 </template>
 
@@ -44,84 +18,68 @@
   export default {
     name: "app",
     data() {
-      const data = [
-        {
-          id: 1,
-          label: '一级 1',
-          children: [{
-            id: 4,
-            label: '二级 1-1',
-            children: [{
-              id: 9,
-              label: '三级 1-1-1'
-            }, {
-              id: 10,
-              label: '三级 1-1-2'
-            }]
-          }]
-        }, {
-          id: 2,
-          label: '一级 2',
-          children: [{
-            id: 5,
-            label: '二级 2-1'
-          }, {
-            id: 6,
-            label: '二级 2-2'
-          }]
-        }, {
-          id: 3,
-          label: '一级 3',
-          children: [{
-            id: 7,
-            label: '二级 3-1'
-          }, {
-            id: 8,
-            label: '二级 3-2'
-          }]
-        }
-      ];
       return {
-        data1: JSON.parse(JSON.stringify(data)),
-        data2: JSON.parse(JSON.stringify(data))
+        filterText: "",
+        data: [
+          {
+            id: 1,
+            label: '一级 1',
+            children: [{
+              id: 4,
+              label: '二级 1-1',
+              children: [{
+                id: 9,
+                label: '三级 1-1-1'
+              }, {
+                id: 10,
+                label: '三级 1-1-2'
+              }]
+            }]
+          }, {
+            id: 2,
+            label: '一级 2',
+            children: [{
+              id: 5,
+              label: '二级 2-1'
+            }, {
+              id: 6,
+              label: '二级 2-2'
+            }]
+          }, {
+            id: 3,
+            label: '一级 3',
+            children: [{
+              id: 7,
+              label: '二级 3-1'
+            }, {
+              id: 8,
+              label: '二级 3-2'
+            }]
+          }
+        ],
+        defaultProps: {
+          children: "children",
+          label: "label"
+        }
       }
     },
     methods: {
-      append(data) {
-        const newChild = {id: id++, label: "testtest", children: []};
-        if (!data.children) {
-          this.$set(data, "children", [])
+      filterNode(value, data) {
+        console.log(value);
+        console.log(data);
+        if (!value) {
+          return true;
         }
-        data.children.push(newChild);
-      },
-      remove(node, data) {
-        const parent =  node.parent;
-        const children = parent.data.children || parent.data;
-        const index = children.findIndex((d) => {d.id == data.id});
-        children.splice(index, 1);
-      },
-      renderContent(h, {node, data, store}) {
-        return (
-          <span class="custom-tree-node">
-            <span>{node.label}</span>
-            <span>
-              <el-button size="mini" type="text" on-click={ () => this.append(data) }>Append</el-button>
-              <el-button size="mini" type="text" on-click={ () => this.remove(node, data) }>Delete</el-button>
-            </span>
-          </span>
-        )
+        return data.label.indexOf(value) !== -1;
+      }
+    },
+    watch: {
+      filterText(val) {
+        this.$refs.tree.filter(val)
       }
     }
   };
 </script>
 
 <style lang="less">
-  .custom-tree-node {
-    flex: 1;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    font-size: 14px;
-    padding-right: 8px;
-  }
 </style>
