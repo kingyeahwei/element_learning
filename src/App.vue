1377c46bf163a2e67fa0e50de2fef5f241713c2e
jinyanwei <jinyanwei@onetarget.cn>
2020-07-15 18:08:51 +0800

    tree 基础用法

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,6 @@
 <template>
   <div id="app">
-    <el-table
-      :data="tableData"
-      height="250"
-      border
-      style="width: 100%">
-      <el-table-column
-        prop="date"
-        label="日期"
-        width="180"
-      ></el-table-column>
-      <el-table-column
-        prop="name"
-        label="姓名"
-        width="180"
-      ></el-table-column>
-      <el-table-column
-        prop="address"
-        label="地址"
-      ></el-table-column>
-    </el-table>
+    <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
   </div>
 </template>
 
@@ -29,40 +10,54 @@
         name: "app",
         data() {
             return {
-                tableData: [
+                data: [
                     {
-                        date: '2016-05-03',
-                        name: '王小虎',
-                        address: '上海市普陀区金沙江路 1518 弄'
+                        label: '一级 1',
+                        children: [{
+                            label: '二级 1-1',
+                            children: [{
+                                label: '三级 1-1-1'
+                            }]
+                        }]
                     }, {
-                        date: '2016-05-02',
-                        name: '王小虎',
-                        address: '上海市普陀区金沙江路 1518 弄'
+                        label: '一级 2',
+                        children: [{
+                            label: '二级 2-1',
+                            children: [{
+                                label: '三级 2-1-1'
+                            }]
+                        }, {
+                            label: '二级 2-2',
+                            children: [{
+                                label: '三级 2-2-1'
+                            }]
+                        }]
                     }, {
-                        date: '2016-05-04',
-                        name: '王小虎',
-                        address: '上海市普陀区金沙江路 1518 弄'
-                    }, {
-                        date: '2016-05-01',
-                        name: '王小虎',
-                        address: '上海市普陀区金沙江路 1518 弄'
-                    }, {
-                        date: '2016-05-08',
-                        name: '王小虎',
-                        address: '上海市普陀区金沙江路 1518 弄'
-                    }, {
-                        date: '2016-05-06',
-                        name: '王小虎',
-                        address: '上海市普陀区金沙江路 1518 弄'
-                    }, {
-                        date: '2016-05-07',
-                        name: '王小虎',
-                        address: '上海市普陀区金沙江路 1518 弄'
+                        label: '一级 3',
+                        children: [{
+                            label: '二级 3-1',
+                            children: [{
+                                label: '三级 3-1-1'
+                            }]
+                        }, {
+                            label: '二级 3-2',
+                            children: [{
+                                label: '三级 3-2-1'
+                            }]
+                        }]
                     }
-                ]
+                ],
+                defaultProps: {
+                    children: "children",
+                    label: "label"
+                }
             }
         },
-        methods: {}
+        methods: {
+            handleNodeClick(data) {
+                console.log(data)
+            }
+        }
     };
 </script>
 
