ff1a1956776905f57df1c25846bd04b771a9ba9c
jinyanwei <jinyanwei@live.com>
2020-07-15 22:38:56 +0800

    tree 手风琴模式

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,11 @@
 <template>
   <div id="app">
-    <el-input v-model="filterText" placeholder="输入关键字过滤"></el-input>
     <el-tree
-      class="filter-tree"
       :data="data"
       :props="defaultProps"
-      default-expand-all
-      :filter-node-method="filterNode"
-      ref="tree"
+      accordion
+      @node-click="handleNodeClick"
     ></el-tree>
-
   </div>
 </template>
 
@@ -19,41 +15,40 @@
     name: "app",
     data() {
       return {
-        filterText: "",
         data: [
           {
-            id: 1,
             label: '一级 1',
             children: [{
-              id: 4,
               label: '二级 1-1',
               children: [{
-                id: 9,
                 label: '三级 1-1-1'
-              }, {
-                id: 10,
-                label: '三级 1-1-2'
               }]
             }]
           }, {
-            id: 2,
             label: '一级 2',
             children: [{
-              id: 5,
-              label: '二级 2-1'
+              label: '二级 2-1',
+              children: [{
+                label: '三级 2-1-1'
+              }]
             }, {
-              id: 6,
-              label: '二级 2-2'
+              label: '二级 2-2',
+              children: [{
+                label: '三级 2-2-1'
+              }]
             }]
           }, {
-            id: 3,
             label: '一级 3',
             children: [{
-              id: 7,
-              label: '二级 3-1'
+              label: '二级 3-1',
+              children: [{
+                label: '三级 3-1-1'
+              }]
             }, {
-              id: 8,
-              label: '二级 3-2'
+              label: '二级 3-2',
+              children: [{
+                label: '三级 3-2-1'
+              }]
             }]
           }
         ],
@@ -64,20 +59,10 @@
       }
     },
     methods: {
-      filterNode(value, data) {
-        console.log(value);
+      handleNodeClick(data) {
         console.log(data);
-        if (!value) {
-          return true;
-        }
-        return data.label.indexOf(value) !== -1;
       }
     },
-    watch: {
-      filterText(val) {
-        this.$refs.tree.filter(val)
-      }
-    }
   };
 </script>
 
