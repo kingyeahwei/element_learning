63fd105878f1a892907006b7acf66232ccb9dae6
jinyanwei <jinyanwei@live.com>
2020-07-15 23:38:21 +0800

    table 单选

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,34 @@
 <template>
   <div id="app">
     <el-table
-      :data="data"
+      ref="singleTable"
+      :data="tableData"
+      highlight-current-row
+      @current-change="handleCurrentChange"
       style="width: 100%">
       <el-table-column
-        prop="date"
-        label="日期"
-        width="150"
+        type="index"
+        width="50"
       ></el-table-column>
-      <el-table-column label="配送信息">
-        <el-table-column
-          prop="name"
-          label="姓名"
-          width="120"
-        ></el-table-column>
-        <el-table-column label="地址">
-          <el-table-column
-            prop="province"
-            label="省份"
-            width="120"
-          ></el-table-column>
-          <el-table-column
-            prop="city"
-            label="市区"
-            width="120"
-          ></el-table-column>
-          <el-table-column
-            prop="address"
-            label="地址"
-            width="300"
-          ></el-table-column>
-          <el-table-column
-            prop="zip"
-            label="邮编"
-          ></el-table-column>
-        </el-table-column>
-      </el-table-column>
+      <el-table-column
+        property="date"
+        label="日期"
+        width="120"
+      ></el-table-column>
+      <el-table-column
+        property="name"
+        label="姓名"
+        width="120"
+      ></el-table-column>
+      <el-table-column
+        property="address"
+        label="地址"
+      ></el-table-column>
     </el-table>
+    <div style="margin-top: 20px">
+      <el-button @click="setCurrent(tableData[1])">选中第二行</el-button>
+      <el-button @click="setCurrent({})">取消选择</el-button>
+    </div>
   </div>
 </template>
 
@@ -45,61 +37,36 @@
     name: "app",
     data() {
       return {
-        data: [
+        tableData: [
           {
-            date: '2016-05-03',
-            name: '王小虎',
-            province: '上海',
-            city: '普陀区',
-            address: '上海市普陀区金沙江路 1518 弄',
-            zip: 200333
-          }, {
             date: '2016-05-02',
             name: '王小虎',
-            province: '上海',
-            city: '普陀区',
-            address: '上海市普陀区金沙江路 1518 弄',
-            zip: 200333
+            address: '上海市普陀区金沙江路 1518 弄'
           }, {
             date: '2016-05-04',
             name: '王小虎',
-            province: '上海',
-            city: '普陀区',
-            address: '上海市普陀区金沙江路 1518 弄',
-            zip: 200333
+            address: '上海市普陀区金沙江路 1517 弄'
           }, {
             date: '2016-05-01',
             name: '王小虎',
-            province: '上海',
-            city: '普陀区',
-            address: '上海市普陀区金沙江路 1518 弄',
-            zip: 200333
+            address: '上海市普陀区金沙江路 1519 弄'
           }, {
-            date: '2016-05-08',
+            date: '2016-05-03',
             name: '王小虎',
-            province: '上海',
-            city: '普陀区',
-            address: '上海市普陀区金沙江路 1518 弄',
-            zip: 200333
-          }, {
-            date: '2016-05-06',
-            name: '王小虎',
-            province: '上海',
-            city: '普陀区',
-            address: '上海市普陀区金沙江路 1518 弄',
-            zip: 200333
-          }, {
-            date: '2016-05-07',
-            name: '王小虎',
-            province: '上海',
-            city: '普陀区',
-            address: '上海市普陀区金沙江路 1518 弄',
-            zip: 200333
+            address: '上海市普陀区金沙江路 1516 弄'
           }
         ],
+        currentRow: null
       }
     },
     methods: {
+      setCurrent(row) {
+        console.log(row)
+        this.$refs.singleTable.setCurrentRow(row)
+      },
+      handleCurrentChange(val) {
+        this.currentRow = val;
+      }
     },
   };
 </script>
