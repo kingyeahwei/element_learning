c11df9af201ab7111975946f3f6a839ea8ef6124
jinyanwei <jinyanwei@live.com>
2020-07-16 23:59:29 +0800

    alert 带辅助性文字介绍

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <div id="app">
-    <el-alert title="成功提示的文案" type="success" center show-icon></el-alert>
-    <el-alert title="消息提示的文案" type="info" center show-icon></el-alert>
-    <el-alert title="警告提示的文案" type="warning" center show-icon></el-alert>
-    <el-alert title="错误提示的文案" type="error" center show-icon></el-alert>
+    <el-alert
+      title="带辅助性文字介绍"
+      description="这是一句绕口令：黑灰化肥会挥发发灰黑化肥挥发；灰黑化肥会挥发发黑灰化肥发挥。 黑灰化肥会挥发发灰黑化肥黑灰挥发化为灰……"
+      type="success">
+    </el-alert>
   </div>
 </template>
 
