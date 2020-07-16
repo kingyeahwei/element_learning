0cdf1564d0e410751cf938f5df9ac04f7325fc81
jinyanwei <jinyanwei@live.com>
2020-07-17 00:22:08 +0800

    loading 整页加载

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,7 @@
 <template>
   <div id="app">
-    <el-table :data="tableData" v-loading="loading" style="width: 100%" element-loading-text="拼命加载中"
-    element-loading-spinner="el-icon-loading"
-    element-loading-background="rgba(0, 0, 0, 0.8)">
-      <el-table-column prop="date" label="日期" width="180"></el-table-column>
-      <el-table-column prop="name" label="姓名" width="180"></el-table-column>
-      <el-table-column prop="address" label="地址"></el-table-column>
-    </el-table>
+    <el-button type="primary" @click="openFullScreen1" v-loading.fullscreen.lock="fullscreenLoading">指令方式</el-button>
+    <el-button type="primary" @click="openFullScreen2">服务方式</el-button>
   </div>
 </template>
 
@@ -15,25 +10,28 @@
     name: "app",
     data() {
       return {
-        tableData: [
-          {
-            date: '2016-05-03',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1518 弄'
-          }, {
-            date: '2016-05-02',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1518 弄'
-          }, {
-            date: '2016-05-04',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1518 弄'
-          }
-        ],
-        loading: true
+        fullscreenLoading: false
       }
     },
-    methods: {}
+    methods: {
+      openFullScreen1() {
+        this.fullscreenLoading = true;
+        setTimeout(() => {
+          this.fullscreenLoading = false;
+        }, 2000)
+      },
+      openFullScreen2() {
+        const loading = this.$loading({
+          lock: true,
+          text: "loading",
+          spinner: "el-icon-loading",
+          background: "rgba(0, 0, 0, 0.7)"
+        })
+        setTimeout(() => {
+          loading.close()
+        }, 2000)
+      }
+    }
   }
 </script>
 
