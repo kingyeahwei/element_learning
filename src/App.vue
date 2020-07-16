5b2d5c0186b04e0fdf9ed61faaaf7f379968dba2
jinyanwei <jinyanwei@live.com>
2020-07-17 00:11:27 +0800

    loading 自定义

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <el-table :data="tableData" v-loading.body="loading" style="width: 100%">
+    <el-table :data="tableData" v-loading="loading" style="width: 100%" element-loading-text="拼命加载中"
+    element-loading-spinner="el-icon-loading"
+    element-loading-background="rgba(0, 0, 0, 0.8)">
       <el-table-column prop="date" label="日期" width="180"></el-table-column>
       <el-table-column prop="name" label="姓名" width="180"></el-table-column>
       <el-table-column prop="address" label="地址"></el-table-column>
