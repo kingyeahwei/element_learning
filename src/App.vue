561ce3a3b5908155e7958a3bc94386d96ef99ce0
jinyanwei <jinyanwei@onetarget.cn>
2020-07-16 13:10:04 +0800

    table 自定义列模板

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,37 @@
 <template>
   <div id="app">
-    <el-button @click="resetDateFilter">清除日期过滤器</el-button>
-    <el-button @click="clearFilter">清除所有过滤器</el-button>
     <el-table
-      ref="filterTable"
       :data="tableData"
       style="width: 100%">
       <el-table-column
-        prop="date"
         label="日期"
-        sortable
-        width="180"
-        column-key="date"
-        :filters="[{ text: '2016-05-01', value: '2016-05-01' }, {text: '2016-05-02', value: '2016-05-02'}]"
-        :filter-method="filterHandler"
-      ></el-table-column>
-      <el-table-column
-        prop="name"
-        label="姓名"
-        width="180"
-      ></el-table-column>
-      <el-table-column
-        prop="tag"
-        label="标签"
-        :filters="[{text: '家', value: '家'}, {text: '公司', value: '公司'}]"
-        :filter-method="filterTag">
+        width="180">
         <template slot-scope="scope">
-          <el-tag
-            :type="scope.row.tag === '家' ? 'primary' : 'success'"
-            disable-transitions
-          >{{scope.row.tag}}
-          </el-tag>
+          <i class="el-icon-time"></i>
+          <span style="margin-left: 10px">{{scope.row.date}}</span>
+        </template>
+      </el-table-column>
+      <el-table-column
+        label="姓名"
+        width="180">
+        <template slot-scope="scope">
+          <el-popover trigger="hover" placement="top">
+            <p>姓名: {{scope.row.name}}</p>
+            <p>住址: {{scope.row.address}}</p>
+            <div slot="reference" class="name-wrapper">
+              <el-tag size="mediun">{{scope.row.name}}</el-tag>
+            </div>
+          </el-popover>
+        </template>
+      </el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <el-button size="mini" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+          <el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
         </template>
       </el-table-column>
     </el-table>
   </div>
-
-
 </template>
 
 <script>
@@ -48,44 +43,29 @@
                     {
                         date: '2016-05-02',
                         name: '王小虎',
-                        address: '上海市普陀区金沙江路 1518 弄',
-                        tag: '家'
+                        address: '上海市普陀区金沙江路 1518 弄'
                     }, {
                         date: '2016-05-04',
                         name: '王小虎',
-                        address: '上海市普陀区金沙江路 1517 弄',
-                        tag: '公司'
+                        address: '上海市普陀区金沙江路 1517 弄'
                     }, {
                         date: '2016-05-01',
                         name: '王小虎',
-                        address: '上海市普陀区金沙江路 1519 弄',
-                        tag: '家'
+                        address: '上海市普陀区金沙江路 1519 弄'
                     }, {
                         date: '2016-05-03',
                         name: '王小虎',
-                        address: '上海市普陀区金沙江路 1516 弄',
-                        tag: '公司'
+                        address: '上海市普陀区金沙江路 1516 弄'
                     }
                 ]
             }
         },
         methods: {
-            resetDateFilter() {
-                this.$refs.filterTable.clearFilter('date');
+            handleEdit(index, row) {
+                console.log(index, row)
             },
-            clearFilter() {
-                this.$refs.filterTable.clearFilter();
-            },
-            formatter(row, column) {
-                return row.address;
-            },
-            filterTag(value, row) {
-                return row.tag === value;
-            },
-            // 参数分别为过滤的值,行对象和列对象
-            filterHandler(value, row, column) {
-                const property = column['property'];
-                return row[property] === value;
+            handleDelete(index, row) {
+                console.log(index, row)
             }
         },
     };
