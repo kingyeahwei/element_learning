182eee1edb47f86954699533fb1079047e064058
jinyanwei <jinyanwei@live.com>
2020-07-16 23:37:36 +0800

    avatar 图片如何适应容器框

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <div id="app">
-    <div class="demo-type">
-      <el-avatar :size="60" src="https://empty" @error="errorHandler">
-        <img src="https://cube.elemecdn.com/e/fd/0fc7d20532fdaf769a25683617711png.png">
-      </el-avatar>
+    <div class="demo-fit">
+      <div class="block" v-for="fit in fits" :key="fit">
+        <span class="title">{{fit}}</span>
+        <el-avatar shape="square" :size="100" :fit="fit" :src="url"></el-avatar>
+      </div>
     </div>
   </div>
 </template>
@@ -13,6 +14,8 @@
     name: "app",
     data() {
       return {
+        fits: ['fill', 'contain', 'cover', 'none', 'scale-down'],
+        url: 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg'
       }
     },
     methods: {
