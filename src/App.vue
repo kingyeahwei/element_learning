2fc73ea10dfef24c0819ef33ac26f9b8cfd67560
jinyanwei <jinyanwei@live.com>
2020-07-16 22:51:30 +0800

    pagination 附加功能

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,51 @@
 <template>
   <div id="app">
-    <el-pagination
-      small
-      layout="sizes, prev, pager, next, jumper"
-      :total="50"
-    ></el-pagination>
+    <div>
+      <span>显示总数</span>
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page.sync="currentPage1"
+        :page-size="100"
+        layout="total, prev, pager, next"
+        :total="1000"
+      ></el-pagination>
+    </div>
+    <div>
+      <span>调整每页显示条数</span>
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page.sync="currentPage2"
+        :page-sizes="[100, 200, 300, 400]"
+        :page-size="100"
+        layout="sizes, prev, pager, next"
+        :total="1000"
+      ></el-pagination>
+    </div>
+    <div>
+      <span>直接前往</span>
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page.sync="currentPage3"
+        :page-size="100"
+        layout="prev, pager, next, jumper"
+        :total="1000"
+      ></el-pagination>
+    </div>
+    <div>
+      <span>完整功能</span>
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page.sync="currentPage4"
+        :page-sizes="[100, 200, 300, 400]"
+        :page-size="100"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="400"
+      ></el-pagination>
+    </div>
   </div>
 </template>
 
@@ -13,9 +54,19 @@
     name: "app",
     data() {
       return {
+        currentPage1: 5,
+        currentPage2: 5,
+        currentPage3: 5,
+        currentPage4: 4,
       }
     },
     methods: {
+      handleSizeChange(value) {
+        console.log(`每页${value}条`);
+      },
+      handleCurrentChange(value) {
+        console.log(`当前页: ${value}`);
+      }
     }
   }
 </script>
