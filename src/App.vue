88a5fecb0dc272b2fd7d00bfb9f971472747f31a
jinyanwei <jinyanwei@live.com>
2020-07-16 23:33:01 +0800

    avatar 图片加载失败的fallback行为

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,9 @@
 <template>
   <div id="app">
     <div class="demo-type">
-      <div>
-        <el-avatar icon="el-icon-user-solid"></el-avatar>
-      </div>
-      <div>
-        <el-avatar src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"></el-avatar>
-      </div>
-      <div>
-        <el-avatar>user</el-avatar>
-      </div>
+      <el-avatar :size="60" src="https://empty" @error="errorHandler">
+        <img src="https://cube.elemecdn.com/e/fd/0fc7d20532fdaf769a25683617711png.png">
+      </el-avatar>
     </div>
   </div>
 </template>
@@ -21,7 +15,11 @@
       return {
       }
     },
-    methods: {}
+    methods: {
+      errorHandler() {
+        return true
+      }
+    }
   }
 </script>
 
