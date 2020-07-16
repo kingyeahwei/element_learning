87b2694bc820a13ec341b56b56223f85322762ef
jinyanwei <jinyanwei@live.com>
2020-07-16 22:14:07 +0800

    pagination 基础用法

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
   <div id="app">
-    <el-progress type="dashboard" :percentage="percentage" :color="colors"></el-progress>
-    <el-button-group>
-      <el-button icon="el-icon-minus" @click="decrease"></el-button>
-      <el-button icon="el-icon-plus" @click="increase"></el-button>
-    </el-button-group>
+    <span>页数比较少时的效果</span>
+    <el-pagination
+      layout="prev, pager, next"
+      :total="50"
+    ></el-pagination>
+    <p>大于7页时的效果</p>
+    <el-pagination
+      layout="prev, pager, next"
+      :total="1000"
+    ></el-pagination>
   </div>
 </template>
 
@@ -13,29 +18,9 @@
     name: "app",
     data() {
       return {
-        percentage: 10,
-        colors: [
-          {color: '#f56c6c', percentage: 20},
-          {color: '#e6a23c', percentage: 40},
-          {color: '#5cb87a', percentage: 60},
-          {color: '#1989fa', percentage: 80},
-          {color: '#6f7ad3', percentage: 100}
-        ]
       }
     },
     methods: {
-      increase() {
-        this.percentage += 10;
-        if (this.percentage > 100) {
-          this.percentage = 100;
-        }
-      },
-      decrease() {
-        this.percentage -= 10;
-        if (this.percentage < 0) {
-          this.percentage = 0;
-        }
-      }
     }
   }
 </script>
