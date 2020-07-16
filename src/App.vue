7e227208c2ceec51920e3fc903c3451e04c33d92
jinyanwei <jinyanwei@live.com>
2020-07-16 21:57:09 +0800

    progress 自定义颜色

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
   <div id="app">
-    <el-progress text-inside :stroke-width="26" :percentage="70"></el-progress>
-    <el-progress text-inside :stroke-width="24" :percentage="100" status="success"></el-progress>
-    <el-progress text-inside :stroke-width="22" :percentage="80" status="warning"></el-progress>
-    <el-progress text-inside :stroke-width="20" :percentage="50" status="exception"></el-progress>
+    <el-progress :percentage="percentage" :color="customColor"></el-progress>
+    <el-progress :percentage="percentage" :color="customColorMethod"></el-progress>
+    <el-progress :percentage="percentage" :color="coustomColors"></el-progress>
+    <el-button-group>
+      <el-button icon="el-icon-minus" @click="decrease"></el-button>
+      <el-button icon="el-icon-plus" @click="increase"></el-button>
+    </el-button-group>
   </div>
 </template>
 
@@ -12,9 +15,39 @@
     name: "app",
     data() {
       return {
+        percentage: 20,
+        customColor: "#409eff",
+        coustomColors: [
+          {color: '#f56c6c', percentage: 20},
+          {color: '#e6a23c', percentage: 40},
+          {color: '#5cb87a', percentage: 60},
+          {color: '#1989fa', percentage: 80},
+          {color: '#6f7ad3', percentage: 100}
+        ]
       }
     },
     methods: {
+      customColorMethod(percentage) {
+        if (percentage < 30) {
+          return "#909399"
+        } else if (percentage < 70) {
+          return "#e6a23c"
+        } else {
+          return "#67c23a"
+        }
+      },
+      increase() {
+        this.percentage += 10;
+        if (this.percentage > 100) {
+          this.percentage == 100;
+        }
+      },
+      decrease() {
+        this.percentage -= 10;
+        if (this.percentage < 0) {
+          this.percentage == 0;
+        }
+      }
     }
   }
 </script>
