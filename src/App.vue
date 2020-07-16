124290d2d4b3157bc72f1094abab195459997a06
jinyanwei <jinyanwei@onetarget.cn>
2020-07-16 17:10:37 +0800

    table 自定义表头

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,19 @@
 <template>
   <div id="app">
-    <el-table :data="tableData" style="width: 100%; margin-bottom: 20px" row-key="id" border default-expand-all
-              :tree-props="{children: 'children', hasChildren: 'hasChildren'}">
-      <el-table-column prop="date" label="日期" sortable width="180"></el-table-column>
-      <el-table-column prop="name" label="姓名" sortable width="180"></el-table-column>
-      <el-table-column prop="address" label="地址"></el-table-column>
-    </el-table>
-
     <el-table
-      :data="tableData1"
-      style="width: 100%"
-      row-key="id"
-      border
-      lazy
-      :load="load"
-      :tree-props="{children: 'children', hasChildren: 'hasChildren'}">
-      <el-table-column prop="date" label="日期" width="180"></el-table-column>
-      <el-table-column prop="name" label="姓名" width="180"></el-table-column>
-      <el-table-column prop="address" label="地址"></el-table-column>
+    :data="filterHandle"
+    style="width: 100%">
+      <el-table-column label="Date" prop="date"></el-table-column>
+      <el-table-column label="Name" prop="name"></el-table-column>
+      <el-table-column align="right">
+        <template slot-scope="scope" slot="header">
+          <el-input v-model="search" size="mini" placeholder="输入关键字搜索"></el-input>
+        </template>
+        <template slot-scope="scope">
+          <el-button size="mini" @click="handleEdit(scope.$index, scope.row)">edit</el-button>
+          <el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">delete</el-button>
+        </template>
+      </el-table-column>
     </el-table>
   </div>
 </template>
@@ -29,83 +25,42 @@
             return {
                 tableData: [
                     {
-                        id: 1,
                         date: '2016-05-02',
-                        name: '王小虎',
+                        name: '王大虎',
                         address: '上海市普陀区金沙江路 1518 弄'
                     }, {
-                        id: 2,
                         date: '2016-05-04',
                         name: '王小虎',
                         address: '上海市普陀区金沙江路 1517 弄'
                     }, {
-                        id: 3,
                         date: '2016-05-01',
                         name: '王小虎',
-                        address: '上海市普陀区金沙江路 1519 弄',
-                        children: [{
-                            id: 31,
-                            date: '2016-05-01',
-                            name: '王小虎',
-                            address: '上海市普陀区金沙江路 1519 弄'
-                        }, {
-                            id: 32,
-                            date: '2016-05-01',
-                            name: '王小虎',
-                            address: '上海市普陀区金沙江路 1519 弄'
-                        }]
+                        address: '上海市普陀区金沙江路 1519 弄'
                     }, {
-                        id: 4,
                         date: '2016-05-03',
                         name: '王小虎',
                         address: '上海市普陀区金沙江路 1516 弄'
                     }
                 ],
-                tableData1: [
-                    {
-                        id: 1,
-                        date: '2016-05-02',
-                        name: '王小虎',
-                        address: '上海市普陀区金沙江路 1518 弄'
-                    }, {
-                        id: 2,
-                        date: '2016-05-04',
-                        name: '王小虎',
-                        address: '上海市普陀区金沙江路 1517 弄'
-                    }, {
-                        id: 3,
-                        date: '2016-05-01',
-                        name: '王小虎',
-                        address: '上海市普陀区金沙江路 1519 弄',
-                        hasChildren: true
-                    }, {
-                        id: 4,
-                        date: '2016-05-03',
-                        name: '王小虎',
-                        address: '上海市普陀区金沙江路 1516 弄'
-                    }
-                ]
+                search: ""
             }
         },
         methods: {
-            load(tree, treeNode, resolve) {
-                setTimeout(() => {
-                    resolve([
-                        {
-                            id: 31,
-                            date: '2016-05-01',
-                            name: '王小虎',
-                            address: '上海市普陀区金沙江路 1519 弄'
-                        }, {
-                            id: 32,
-                            date: '2016-05-01',
-                            name: '王小虎',
-                            address: '上海市普陀区金沙江路 1519 弄'
-                        }
-                    ])
-                }, 1000)
-            }
+            handleEdit(index, row) {
+                console.log(index, row);
+            },
+            handleDelete(index, row) {
+                console.log(index, row);
+            },
         },
+        computed: {
+            filterHandle() {
+                let _this = this
+                return this.tableData.filter((data) => {
+                    return  !_this.search || data.name.toLowerCase().includes(_this.search.toLowerCase())
+                })
+            }
+        }
     };
 </script>
 
