c1015de5366d498e127dd4ba6fe63f6dbbbe6d70
jinyanwei <jinyanwei@live.com>
2020-07-16 23:48:14 +0800

    alert 主题

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
-    <el-alert title="成功提示的文案" type="success"></el-alert>
-    <el-alert title="消息提示的文案" type="info"></el-alert>
-    <el-alert title="警告提示的文案" type="warning"></el-alert>
-    <el-alert title="错误提示的文案" type="error"></el-alert>
+    <el-alert title="成功提示的文案" type="success" effect="dark"></el-alert>
+    <el-alert title="消息提示的文案" type="info" effect="dark"></el-alert>
+    <el-alert title="警告提示的文案" type="warning" effect="dark"></el-alert>
+    <el-alert title="错误提示的文案" type="error" effect="dark"></el-alert>
   </div>
 </template>
 
