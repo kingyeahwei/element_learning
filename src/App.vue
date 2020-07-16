346be329620ba54741f9e067280a6cf039e13dc3
jinyanwei <jinyanwei@onetarget.cn>
2020-07-16 13:21:01 +0800

    table 展开行

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,36 @@
 <template>
   <div id="app">
-    <el-table
-      :data="tableData"
-      style="width: 100%">
-      <el-table-column
-        label="日期"
-        width="180">
-        <template slot-scope="scope">
-          <i class="el-icon-time"></i>
-          <span style="margin-left: 10px">{{scope.row.date}}</span>
+    <el-table :data="tableData" style="width: 100%">
+      <el-table-column type="expand">
+        <template slot-scope="props">
+          <el-form label-position="left" inline class="demo-table-expand">
+            <el-form-item label="商品名称">
+              <span>{{props.row.name}}</span>
+            </el-form-item>
+            <el-form-item label="所属店铺">
+              <span>{{props.row.shop}}</span>
+            </el-form-item>
+            <el-form-item label="商品ID">
+              <span>{{props.row.id}}</span>
+            </el-form-item>
+            <el-form-item label="店铺ID">
+              <span>{{props.row.shopId}}</span>
+            </el-form-item>
+            <el-form-item label="商品分类">
+              <span>{{props.row.category}}</span>
+            </el-form-item>
+            <el-form-item label="店铺地址">
+              <span>{{props.row.address}}</span>
+            </el-form-item>
+            <el-form-item label="商品描述">
+              <span>{{props.row.desc}}</span>
+            </el-form-item>
+          </el-form>
         </template>
       </el-table-column>
-      <el-table-column
-        label="姓名"
-        width="180">
-        <template slot-scope="scope">
-          <el-popover trigger="hover" placement="top">
-            <p>姓名: {{scope.row.name}}</p>
-            <p>住址: {{scope.row.address}}</p>
-            <div slot="reference" class="name-wrapper">
-              <el-tag size="mediun">{{scope.row.name}}</el-tag>
-            </div>
-          </el-popover>
-        </template>
-      </el-table-column>
-      <el-table-column label="操作">
-        <template slot-scope="scope">
-          <el-button size="mini" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
-          <el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
-        </template>
-      </el-table-column>
+      <el-table-column label="商品id" prop="id"></el-table-column>
+      <el-table-column label="商品名称" prop="name"></el-table-column>
+      <el-table-column label="描述" prop="desc"></el-table-column>
     </el-table>
   </div>
 </template>
@@ -41,35 +42,58 @@
             return {
                 tableData: [
                     {
-                        date: '2016-05-02',
-                        name: '王小虎',
-                        address: '上海市普陀区金沙江路 1518 弄'
+                        id: '12987122',
+                        name: '好滋好味鸡蛋仔',
+                        category: '江浙小吃、小吃零食',
+                        desc: '荷兰优质淡奶，奶香浓而不腻',
+                        address: '上海市普陀区真北路',
+                        shop: '王小虎夫妻店',
+                        shopId: '10333'
                     }, {
-                        date: '2016-05-04',
-                        name: '王小虎',
-                        address: '上海市普陀区金沙江路 1517 弄'
+                        id: '12987123',
+                        name: '好滋好味鸡蛋仔',
+                        category: '江浙小吃、小吃零食',
+                        desc: '荷兰优质淡奶，奶香浓而不腻',
+                        address: '上海市普陀区真北路',
+                        shop: '王小虎夫妻店',
+                        shopId: '10333'
                     }, {
-                        date: '2016-05-01',
-                        name: '王小虎',
-                        address: '上海市普陀区金沙江路 1519 弄'
+                        id: '12987125',
+                        name: '好滋好味鸡蛋仔',
+                        category: '江浙小吃、小吃零食',
+                        desc: '荷兰优质淡奶，奶香浓而不腻',
+                        address: '上海市普陀区真北路',
+                        shop: '王小虎夫妻店',
+                        shopId: '10333'
                     }, {
-                        date: '2016-05-03',
-                        name: '王小虎',
-                        address: '上海市普陀区金沙江路 1516 弄'
+                        id: '12987126',
+                        name: '好滋好味鸡蛋仔',
+                        category: '江浙小吃、小吃零食',
+                        desc: '荷兰优质淡奶，奶香浓而不腻',
+                        address: '上海市普陀区真北路',
+                        shop: '王小虎夫妻店',
+                        shopId: '10333'
                     }
                 ]
             }
         },
-        methods: {
-            handleEdit(index, row) {
-                console.log(index, row)
-            },
-            handleDelete(index, row) {
-                console.log(index, row)
-            }
-        },
+        methods: {},
     };
 </script>
 
 <style lang="less">
+  .demo-table-expand {
+    font-size: 0;
+  }
+
+  .demo-table-expand label {
+    width: 90px;
+    color: #99a9bf;
+  }
+
+  .demo-table-expand .el-form-item {
+    margin-right: 0;
+    margin-bottom: 0;
+    width: 50%;
+  }
 </style>
