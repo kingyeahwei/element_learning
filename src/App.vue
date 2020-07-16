b1cee7800b023f1b9959c74a788cbcf9bb41a9c6
jinyanwei <jinyanwei@onetarget.cn>
2020-07-16 10:14:31 +0800

    table 过滤

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,38 @@
 <template>
   <div id="app">
+    <el-button @click="resetDateFilter">清除日期过滤器</el-button>
+    <el-button @click="clearFilter">清除所有过滤器</el-button>
     <el-table
+      ref="filterTable"
       :data="tableData"
-      style="width: 100%"
-      :default-sort="{prop: 'date', order: 'desending'}">
+      style="width: 100%">
       <el-table-column
         prop="date"
         label="日期"
         sortable
         width="180"
+        column-key="date"
+        :filters="[{ text: '2016-05-01', value: '2016-05-01' }, {text: '2016-05-02', value: '2016-05-02'}]"
+        :filter-method="filterHandler"
       ></el-table-column>
       <el-table-column
         prop="name"
         label="姓名"
-        sortable
         width="180"
       ></el-table-column>
       <el-table-column
-        prop="address"
-        label="地址"
-        :formatter="formatter"
-      ></el-table-column>
+        prop="tag"
+        label="标签"
+        :filters="[{text: '家', value: '家'}, {text: '公司', value: '公司'}]"
+        :filter-method="filterTag">
+        <template slot-scope="scope">
+          <el-tag
+            :type="scope.row.tag === '家' ? 'primary' : 'success'"
+            disable-transitions
+          >{{scope.row.tag}}
+          </el-tag>
+        </template>
+      </el-table-column>
     </el-table>
   </div>
 
@@ -28,37 +40,55 @@
 </template>
 
 <script>
-  export default {
-    name: "app",
-    data() {
-      return {
-        tableData: [
-          {
-            date: '2016-05-02',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1518 弄'
-          }, {
-            date: '2016-05-04',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1517 弄'
-          }, {
-            date: '2016-05-01',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1519 弄'
-          }, {
-            date: '2016-05-03',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1516 弄'
-          }
-        ]
-      }
-    },
-    methods: {
-      formatter(row, column) {
-        return row.address
-      }
-    },
-  };
+    export default {
+        name: "app",
+        data() {
+            return {
+                tableData: [
+                    {
+                        date: '2016-05-02',
+                        name: '王小虎',
+                        address: '上海市普陀区金沙江路 1518 弄',
+                        tag: '家'
+                    }, {
+                        date: '2016-05-04',
+                        name: '王小虎',
+                        address: '上海市普陀区金沙江路 1517 弄',
+                        tag: '公司'
+                    }, {
+                        date: '2016-05-01',
+                        name: '王小虎',
+                        address: '上海市普陀区金沙江路 1519 弄',
+                        tag: '家'
+                    }, {
+                        date: '2016-05-03',
+                        name: '王小虎',
+                        address: '上海市普陀区金沙江路 1516 弄',
+                        tag: '公司'
+                    }
+                ]
+            }
+        },
+        methods: {
+            resetDateFilter() {
+                this.$refs.filterTable.clearFilter('date');
+            },
+            clearFilter() {
+                this.$refs.filterTable.clearFilter();
+            },
+            formatter(row, column) {
+                return row.address;
+            },
+            filterTag(value, row) {
+                return row.tag === value;
+            },
+            // 参数分别为过滤的值,行对象和列对象
+            filterHandler(value, row, column) {
+                const property = column['property'];
+                return row[property] === value;
+            }
+        },
+    };
 </script>
 
 <style lang="less">
