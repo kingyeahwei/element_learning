b1d212f0c092db9986c474cdb4b771b3e8958682
jinyanwei <jinyanwei@live.com>
2020-07-16 21:27:47 +0800

    table 自定义索引

--- src/App.vue
+++ src/App.vue
@@ -2,113 +2,77 @@
   <div id="app">
     <el-table
       :data="tableData"
-      :span-method="arraySpanMethod"
-      border
       style="width: 100%">
-      <el-table-column prop="id" label="ID" width="180"></el-table-column>
-      <el-table-column prop="name" label="姓名"></el-table-column>
-      <el-table-column prop="amount1" label="数值1" sortable></el-table-column>
-      <el-table-column prop="amount2" label="数值2" sortable></el-table-column>
-      <el-table-column prop="amount3" label="数值3" sortable></el-table-column>
-    </el-table>
-
-    <el-table
-      :data="tableData"
-      :span-method="objectSpanMethod"
-      border
-      style="width: 100%; margin-top: 20px">
       <el-table-column
-        prop="id"
-        label="ID"
-        width="180">
-      </el-table-column>
+        type="index"
+        :index="indexMethod"
+      ></el-table-column>
+      <el-table-column
+        prop="date"
+        label="日期"
+        width="180"
+      ></el-table-column>
       <el-table-column
         prop="name"
-        label="姓名">
-      </el-table-column>
+        label="姓名"
+        width="180"
+      ></el-table-column>
       <el-table-column
-        prop="amount1"
-        label="数值 1（元）">
-      </el-table-column>
-      <el-table-column
-        prop="amount2"
-        label="数值 2（元）">
-      </el-table-column>
-      <el-table-column
-        prop="amount3"
-        label="数值 3（元）">
-      </el-table-column>
+        prop="address"
+        label="地址"
+      ></el-table-column>
     </el-table>
   </div>
 </template>
 
 <script>
-    export default {
-        name: "app",
-        data() {
-            return {
-                tableData: [
-                    {
-                        id: '12987122',
-                        name: '王小虎',
-                        amount1: '234',
-                        amount2: '3.2',
-                        amount3: 10
-                    }, {
-                        id: '12987123',
-                        name: '王小虎',
-                        amount1: '165',
-                        amount2: '4.43',
-                        amount3: 12
-                    }, {
-                        id: '12987124',
-                        name: '王小虎',
-                        amount1: '324',
-                        amount2: '1.9',
-                        amount3: 9
-                    }, {
-                        id: '12987125',
-                        name: '王小虎',
-                        amount1: '621',
-                        amount2: '2.2',
-                        amount3: 17
-                    }, {
-                        id: '12987126',
-                        name: '王小虎',
-                        amount1: '539',
-                        amount2: '4.1',
-                        amount3: 15
-                    }
-                ]
-            }
-        },
-        methods: {
-            arraySpanMethod({row, column, rowIndex, columnIndex}) {
-                if (rowIndex % 2 === 0) {
-                    if (columnIndex === 0) {
-                        return [1, 2];
-                    } else if (columnIndex === 1) {
-                        return [0, 0];
-                    }
-                }
-            },
-            objectSpanMethod({ row, column, rowIndex, columnIndex }) {
-                if (columnIndex === 0) {
-                    if (rowIndex === 1) {
-                        return {
-                            rowspan: 2,
-                            colspan: 1
-                        };
-                    } else if (rowIndex === 2) {
-                        return {
-                            rowspan: 0,
-                            colspan: 0
-                        };
-                    }
-                }
-            }
-        },
-    };
+  export default {
+    name: "app",
+    data() {
+      return {
+        tableData: [
+          {
+            date: '2016-05-02',
+            name: '王小虎',
+            province: '上海',
+            city: '普陀区',
+            address: '上海市普陀区金沙江路 1518 弄',
+            zip: 200333,
+            tag: '家'
+          }, {
+            date: '2016-05-04',
+            name: '王小虎',
+            province: '上海',
+            city: '普陀区',
+            address: '上海市普陀区金沙江路 1517 弄',
+            zip: 200333,
+            tag: '公司'
+          }, {
+            date: '2016-05-01',
+            name: '王小虎',
+            province: '上海',
+            city: '普陀区',
+            address: '上海市普陀区金沙江路 1519 弄',
+            zip: 200333,
+            tag: '家'
+          }, {
+            date: '2016-05-03',
+            name: '王小虎',
+            province: '上海',
+            city: '普陀区',
+            address: '上海市普陀区金沙江路 1516 弄',
+            zip: 200333,
+            tag: '公司'
+          }
+        ]
+      }
+    },
+    methods: {
+      indexMethod(index) {
+        return index + 1;
+      }
+    }
+  }
 </script>
 
 <style lang="less">
