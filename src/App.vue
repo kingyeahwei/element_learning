09f2b2ff89cb0c053b9b14b761d012298f344c79
jinyanwei <jinyanwei@live.com>
2020-07-16 23:57:35 +0800

    alert 文字居中

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
-    <el-alert title="成功提示的文案" type="success" show-icon></el-alert>
-    <el-alert title="消息提示的文案" type="info" show-icon></el-alert>
-    <el-alert title="警告提示的文案" type="warning" show-icon></el-alert>
-    <el-alert title="错误提示的文案" type="error" show-icon></el-alert>
+    <el-alert title="成功提示的文案" type="success" center show-icon></el-alert>
+    <el-alert title="消息提示的文案" type="info" center show-icon></el-alert>
+    <el-alert title="警告提示的文案" type="warning" center show-icon></el-alert>
+    <el-alert title="错误提示的文案" type="error" center show-icon></el-alert>
   </div>
 </template>
 
