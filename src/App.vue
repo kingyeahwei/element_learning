204e92097ac82b7bd170ed79ed65a997eedd39f3
jinyanwei <jinyanwei@live.com>
2020-07-16 23:13:08 +0800

    badge 最大值

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,17 @@
 <template>
   <div id="app">
-    <el-badge value="12" class="item">
+    <el-badge
+      :value="200"
+      :max="99"
+      class="item">
       <el-button size="small">评论</el-button>
     </el-badge>
-    <el-badge value="3" class="item">
+    <el-badge
+      :value="100"
+      :max="10"
+      class="item">
       <el-button size="small">回复</el-button>
     </el-badge>
-    <el-badge value="1" class="item" type="primary">
-      <el-button size="small">评论</el-button>
-    </el-badge>
-    <el-badge value="2" class="item" type="warning">
-      <el-button size="small">回复</el-button>
-    </el-badge>
-    <el-dropdown trigger="click">
-      <span class="el-dropdown-link">
-        点我查看<i class="el-icon-caret-bottom el-icon-right"></i>
-      </span>
-      <el-dropdown-menu slot="dropdown">
-        <el-dropdown-item>
-          <el-badge value="12">
-            <el-button type="text">评论</el-button>
-          </el-badge>
-        </el-dropdown-item>
-        <el-dropdown-item>
-          回复
-          <el-badge value="3"></el-badge>
-        </el-dropdown-item>
-      </el-dropdown-menu>
-    </el-dropdown>
   </div>
 </template>
 
