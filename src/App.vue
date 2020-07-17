20d4f43e9dbe7e8a43537daa08a979a4ce187251
jinyanwei <jinyanwei@live.com>
2020-07-17 23:27:47 +0800

    carousel 卡片化

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <template>
   <div id="app">
-    <el-carousel :interval="5000" arrow="always">
-      <el-carousel-item
-        v-for="item in 4"
-        :key="item">
-        <h3>{{item}}</h3>
+    <el-carousel :interval="4000" type="card" height="200px">
+      <el-carousel-item v-for="item in 6" :key="item">
+        <h3 class="medium">{{item}}</h3>
       </el-carousel-item>
     </el-carousel>
   </div>
@@ -23,14 +21,14 @@
 <style lang="less">
   .el-carousel__item h3 {
     color: #475669;
-    font-size: 18px;
+    font-size: 14px;
     opacity: 0.75;
-    line-height: 300px;
+    line-height: 200px;
     margin: 0;
     text-align: center;
   }
   .el-carousel__item:nth-child(2n) {
-    background-color: #99a9df;
+    background-color: #99a9bf;
   }
   .el-carousel__item:nth-child(2n + 1) {
     background-color: #d3dce6;
