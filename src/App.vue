b19dfe419964a6b9ef360638913b265935d28caf
jinyanwei <jinyanwei@onetarget.cn>
2020-07-17 15:59:45 +0800

    steps 居中的步骤条

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <div id="app">
-    <el-steps :active="1">
-      <el-step title="步骤1" description="这是一段很长很长很长的描述性文字这是一段很长很长很长的描述性文字"></el-step>
-      <el-step title="步骤2" description="这是一段很长很长很长的描述性文字这是一段很长很长很长的描述性文字"></el-step>
-      <el-step title="步骤3" description="这是一段很长很长很长的描述性文字这是一段很长很长很长的描述性文字"></el-step>
+    <el-steps :active="2" align-center finish-status="error">
+      <el-step title="步骤1" description="走上社会发斯蒂芬斯蒂芬地方"></el-step>
+      <el-step title="步骤2" description="走上社会发斯蒂芬斯蒂芬地方"></el-step>
+      <el-step title="步骤3" description="走上社会发斯蒂芬斯蒂芬地方"></el-step>
+      <el-step title="步骤4" description="走上社会发斯蒂芬斯蒂芬地方"></el-step>
     </el-steps>
   </div>
 </template>
@@ -17,11 +18,6 @@
             }
         },
         methods: {
-            next() {
-                if (this.active++ > 3) {
-                    this.active = 0;
-                }
-            }
         }
     }
 </script>
