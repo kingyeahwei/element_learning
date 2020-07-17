89e7f940b4c832c3aef9b555b19168888d638590
jinyanwei <jinyanwei@live.com>
2020-07-17 23:55:39 +0800

    collapse 自定义面板标题

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="app">
-    <el-collapse v-model="activename" accordion @change="handleChange">
-      <el-collapse-item title="一致性" name="1">
+    <el-collapse v-model="activename" accordion>
+      <el-collapse-item name="1">
+        <template slot="title" >
+          一致性 <i class="header-icon el-icon-info"></i>
+        </template>
         <div>与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；</div>
         <div>在界面中一致：所有的元素和结构需保持一致，比如：设计样式、图标和文本、元素的位置等。</div>
       </el-collapse-item>
