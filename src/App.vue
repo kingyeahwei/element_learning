6e6075affdf5723ce0dbe3beb0f7ef521f8be7de
jinyanwei <jinyanwei@onetarget.cn>
2020-07-17 15:34:06 +0800

    dropdown 菜单隐藏方式

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,17 @@
 <template>
   <div id="app">
-    <el-row class="block-col-2">
-      <el-col :span="12">
-        <span class="demonstration">hover 激活</span><br>
-        <el-dropdown>
-          <span class="el-dropdown-link">
-            下拉菜单<i class="el-icon-arrow-down el-icon--right"></i>
-          </span>
-          <el-dropdown-menu slot="dropdown">
-            <el-dropdown-item icon="el-icon-plus">黄金糕</el-dropdown-item>
-            <el-dropdown-item icon="el-icon-circle-plus">狮子头</el-dropdown-item>
-            <el-dropdown-item icon="el-icon-circle-plus-outline">螺蛳粉</el-dropdown-item>
-            <el-dropdown-item icon="el-icon-check">双皮奶</el-dropdown-item>
-            <el-dropdown-item icon="el-icon-circle-check">蚵煎仔</el-dropdown-item>
-          </el-dropdown-menu>
-        </el-dropdown>
-      </el-col>
-      <el-col :span="12">
-        <span class="demonstration">click 激活</span><br>
-        <el-dropdown trigger="click">
-          <span class="el-dropdown-link">
-            下拉菜单 <i class="el-icon-arrow-down el-icon--right"></i>
-          </span>
-          <el-dropdown-menu slot="dropdown">
-            <el-dropdown-item icon="el-icon-plus">黄金糕</el-dropdown-item>
-            <el-dropdown-item icon="el-icon-circle-plus">狮子头</el-dropdown-item>
-            <el-dropdown-item icon="el-icon-circle-plus-outline">螺蛳粉</el-dropdown-item>
-            <el-dropdown-item icon="el-icon-check">双皮奶</el-dropdown-item>
-            <el-dropdown-item icon="el-icon-circle-check">蚵煎仔</el-dropdown-item>
-          </el-dropdown-menu>
-        </el-dropdown>
-      </el-col>
-    </el-row>
+    <el-dropdown :hide-on-click="false">
+      <span class="el-dropdown-link">
+        下拉菜单 <i class="el-icon-arrow-down el-icon--right"></i>
+      </span>
+      <el-dropdown-menu slot="dropdown">
+        <el-dropdown-item>黄金糕</el-dropdown-item>
+        <el-dropdown-item>黄金糕</el-dropdown-item>
+        <el-dropdown-item>黄金糕</el-dropdown-item>
+        <el-dropdown-item disabled>黄金糕</el-dropdown-item>
+        <el-dropdown-item divided>黄金糕</el-dropdown-item>
+      </el-dropdown-menu>
+    </el-dropdown>
   </div>
 </template>
 
@@ -50,15 +30,9 @@
 <style lang="less">
   .el-dropdown-link {
     cursor: pointer;
-    color: #409eff;
+    color: blue;
   }
   .el-icon-arrow-down {
-    font-size: 12px;
-  }
-  .demonstration {
-    display: block;
-    color: #8492a6;
-    font-size: 14px;
-    margin-bottom: 20px;
+    color: blue;
   }
 </style>
