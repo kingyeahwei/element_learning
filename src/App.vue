bd0d157f47b09af8602489b95613d3b11556919d
jinyanwei <jinyanwei@onetarget.cn>
2020-07-17 09:57:25 +0800

    message 基础用法

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,32 @@
 <template>
   <div id="app">
-    <el-button type="primary" @click="openFullScreen1" v-loading.fullscreen.lock="fullscreenLoading">指令方式</el-button>
-    <el-button type="primary" @click="openFullScreen2">服务方式</el-button>
+    <el-button :plain="true" @click="open">打开消息提示</el-button>
+    <el-button :plain="true" @click="openVn">Vnode</el-button>
   </div>
 </template>
 
 <script>
-  export default {
-    name: "app",
-    data() {
-      return {
-        fullscreenLoading: false
-      }
-    },
-    methods: {
-      openFullScreen1() {
-        this.fullscreenLoading = true;
-        setTimeout(() => {
-          this.fullscreenLoading = false;
-        }, 2000)
-      },
-      openFullScreen2() {
-        const loading = this.$loading({
-          lock: true,
-          text: "loading",
-          spinner: "el-icon-loading",
-          background: "rgba(0, 0, 0, 0.7)"
-        })
-        setTimeout(() => {
-          loading.close()
-        }, 2000)
-      }
+    export default {
+        name: "app",
+        data() {
+            return {
+            }
+        },
+        methods: {
+            open() {
+                this.$message("这是一条消息提示");
+            },
+            openVn() {
+                const h = this.$createElement;
+                this.$message({
+                    message: h("p", null, [
+                        h("span", null, "内容可以是"),
+                        h("i", {style: "color: teal"}, "VNode")
+                    ])
+                })
+            }
+        }
     }
-  }
 </script>
 
 <style lang="less">
