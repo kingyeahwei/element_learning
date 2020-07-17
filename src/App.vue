bd7655fc16254d7bcd8509456eeb2d8189d5444c
jinyanwei <jinyanwei@onetarget.cn>
2020-07-17 14:44:51 +0800

    breadcrumb 基础用法

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,11 @@
 <template>
   <div id="app">
-    <div style="margin-bottom: 20px">
-      <el-button
-        size="small"
-        @click="addTab(editableTabsValue)">
-        add tab
-      </el-button>
-    </div>
-    <el-tabs v-model="editableTabsValue" type="card" closable @tab-remove="removeTab">
-      <el-tab-pane
-        v-for="(item, index) in editableTabs"
-        :key="item.name"
-        :label="item.title"
-        :name="item.name">
-        {{item.content}}
-      </el-tab-pane>
-    </el-tabs>
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item :to="{path: '/'}">首页</el-breadcrumb-item>
+      <el-breadcrumb-item><a href="/">活动管理</a></el-breadcrumb-item>
+      <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+      <el-breadcrumb-item>活动列表</el-breadcrumb-item>
+    </el-breadcrumb>
   </div>
 </template>
 
@@ -24,61 +14,9 @@
         name: "app",
         data() {
             return {
-                editableTabsValue: '2',
-                editableTabs: [
-                    {
-                        title: 'Tab 1',
-                        name: '1',
-                        content: 'Tab 1 content'
-                    }, {
-                        title: 'Tab 2',
-                        name: '2',
-                        content: 'Tab 2 content'
-                    }, {
-                        title: 'Tab 3',
-                        name: '3',
-                        content: 'Tab 3 content'
-                    }, {
-                        title: 'Tab 4',
-                        name: '4',
-                        content: 'Tab 4 content'
-                    }
-                ],
-                tabIndex: 2
             }
         },
         methods: {
-            addTab(targetName) {
-                let newTabName = ++this.tabIndex + "";
-                this.editableTabs.push({
-                    title: "new tab",
-                    name: newTabName,
-                    content: "new tab content"
-                })
-                this.editableTabsValue = newTabName
-            },
-            removeTab(targetName) {
-                let tabs = this.editableTabs;
-                let activeName = this.editableTabsValue;
-                // 更改那个被选中
-                if (activeName === targetName) {
-                    tabs.forEach((tab, index) => {
-                        if (tab.name === targetName) {
-                            let nextTab = tabs[index + 1] || tabs[index - 1];
-                            if (nextTab) {
-                                activeName = nextTab.name;
-                            }
-                        }
-                    });
-                }
-
-                this.editableTabs = tabs.filter(tab => tab.name !== targetName);
-                if (this.editableTabs.length > 0) {
-                  this.editableTabsValue = activeName;
-                } else  {
-                    this.editableTabsValue = ""
-                }
-            }
         }
     }
 </script>
