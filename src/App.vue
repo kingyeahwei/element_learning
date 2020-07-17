c988bd6afa600953b0813c757743cf34ce2a4fbd
jinyanwei <jinyanwei@live.com>
2020-07-18 02:00:07 +0800

    backtop 回到顶部

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,23 @@
 <template>
   <div id="app">
-    <div class="demo-image-error">
-      <div class="block">
-        <span>默认</span>
-        <el-image></el-image>
-      </div>
-      <div class="block">
-        <span>自定义</span>
-        <el-image>
-          <div slot="error" class="image-slot">
-            <i class="el-icon-picture-outline"></i>
-          </div>
-        </el-image>
-      </div>
+    <div class="wrap" style="height: 500px; width: 100%; border: 1px solid blue;overflow-y: auto">
+      <div style="height: 2000px;background: red;"></div>
     </div>
+    <el-backtop :bottom="100" :right="100" target=".wrap">
+      <div
+        style="{
+        height: 100%;
+        width: 100%;
+        background-color: #f2f5f6;
+        box-shadow: 0 0 6px rgba(0,0,0, .12);
+        text-align: center;
+        line-height: 40px;
+        color: #1989fa;
+      }"
+      >
+        UP
+      </div>
+    </el-backtop>
   </div>
 </template>
 
@@ -21,16 +25,14 @@
   export default {
     name: "app",
     data() {
-      return {
-      }
+      return {}
     },
-    methods: {
-    }
+    methods: {}
   }
 </script>
 
 <style lang="less">
-  .is-selected {
-    color: #1989fa;
-  }
+  /*#app {*/
+  /*  height: 2000px;*/
+  /*}*/
 </style>
