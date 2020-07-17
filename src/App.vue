c54238ebf84315fba0bc0e7eaa97a8b846ee3dc9
jinyanwei <jinyanwei@onetarget.cn>
2020-07-17 15:39:43 +0800

    dropdown 指令事件

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <div id="app">
-    <el-dropdown :hide-on-click="false">
+    <el-dropdown @command="handleCommand">
       <span class="el-dropdown-link">
         下拉菜单 <i class="el-icon-arrow-down el-icon--right"></i>
       </span>
       <el-dropdown-menu slot="dropdown">
-        <el-dropdown-item>黄金糕</el-dropdown-item>
-        <el-dropdown-item>黄金糕</el-dropdown-item>
-        <el-dropdown-item>黄金糕</el-dropdown-item>
-        <el-dropdown-item disabled>黄金糕</el-dropdown-item>
-        <el-dropdown-item divided>黄金糕</el-dropdown-item>
+        <el-dropdown-item command="a">华金糕</el-dropdown-item>
+        <el-dropdown-item command="b">狮子头</el-dropdown-item>
+        <el-dropdown-item command="c">螺蛳粉</el-dropdown-item>
+        <el-dropdown-item command="d">蒜片</el-dropdown-item>
+        <el-dropdown-item command="e">蚵仔煎</el-dropdown-item>
       </el-dropdown-menu>
     </el-dropdown>
   </div>
@@ -23,6 +23,10 @@
             }
         },
         methods: {
+            handleCommand(command) {
+                console.log(command)
+                this.$message("click on item" + command);
+            }
         }
     }
 </script>
