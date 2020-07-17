e7ff9b8d532554d7f5d4c9c3b917b865bc75034e
jinyanwei <jinyanwei@onetarget.cn>
2020-07-17 16:37:54 +0800

    dialog 自定义内容

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,31 @@
 <template>
   <div id="app">
-    <el-button type="text" @click="dialogVisible = true">点击打开 dialog</el-button>
-    <el-dialog
-      title="提示"
-      :visible.sync="dialogVisible"
-      width="30%"
-      :before-close="handleClose">
-      <span>这是一段信息</span>
-      <span slot="footer" class="dialog-footer">
-        <el-button @click="dialogVisible = false">取消</el-button>
-        <el-button type="primary" @click="dialogVisible = false">确定</el-button>
-      </span>
+    <el-button type="text" @click="dialogTableVisible = true">打开嵌套表格的dialog</el-button>
+    <el-dialog title="收货地址" :visible.sync="dialogTableVisible">
+      <el-table :data="gridData">
+        <el-table-column property="date" label="日期" width="150"></el-table-column>
+        <el-table-column property="name" label="姓名" width="200"></el-table-column>
+        <el-table-column property="address" label="地址"></el-table-column>
+      </el-table>
+    </el-dialog>
+
+    <el-button type="text" @click="dialogFormVisible = true">打开嵌套表单的dialog</el-button>
+    <el-dialog title="收货地址" :visible.sync="dialogFormVisible">
+      <el-form :model="form">
+        <el-form-item label="活动名称" :label-width="formLabelWidth">
+          <el-input v-model="form.name" auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="活动区域" :label-width="formLabelWidth">
+          <el-select v-model="form.region" placeholder="请选择活动区域">
+            <el-option label="区域1" value="shanghai"></el-option>
+            <el-option label="区域2" value="beijing"></el-option>
+          </el-select>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取消</el-button>
+        <el-button @click="dialogFormVisible = false" type="primary">确定</el-button>
+      </div>
     </el-dialog>
   </div>
 </template>
@@ -20,7 +35,38 @@
         name: "app",
         data() {
             return {
-                dialogVisible: false
+                dialogTableVisible: false,
+                dialogFormVisible: false,
+                gridData: [
+                    {
+                        date: '2016-05-02',
+                        name: '王小虎',
+                        address: '上海市普陀区金沙江路 1518 弄'
+                    }, {
+                        date: '2016-05-04',
+                        name: '王小虎',
+                        address: '上海市普陀区金沙江路 1518 弄'
+                    }, {
+                        date: '2016-05-01',
+                        name: '王小虎',
+                        address: '上海市普陀区金沙江路 1518 弄'
+                    }, {
+                        date: '2016-05-03',
+                        name: '王小虎',
+                        address: '上海市普陀区金沙江路 1518 弄'
+                    }
+                ],
+                form: {
+                    name: "",
+                    region: "",
+                    date1: "",
+                    date2: "",
+                    delivery: false,
+                    type: [],
+                    resource: "",
+                    desc: ""
+                },
+                formLabelWidth: "120px"
             }
         },
         methods: {
