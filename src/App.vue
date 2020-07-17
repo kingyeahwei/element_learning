18d37098c311b20f1aa8e3aa209e666bfe1b8cb1
jinyanwei <jinyanwei@live.com>
2020-07-18 00:25:12 +0800

    timeline 自定义时间戳

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,25 @@
 <template>
   <div id="app">
-    <div class="block">
-      <el-timeline>
-        <el-timeline-item
-          v-for="(activity,index) in activities"
-          :key="index"
-          :icon="activity.icon"
-          :type="activity.type"
-          :color="activity.color"
-          :size="activity.size"
-          :timestamp="activity.timestamp">
-          {{activity.content}}
-        </el-timeline-item>
-      </el-timeline>
-    </div>
+    <el-timeline>
+      <el-timeline-item timestamp="2018/4/12" placement="top">
+        <el-card>
+          <h4>更新 Github 模板</h4>
+          <p>王小虎 提交于 2018/4/12 20:46</p>
+        </el-card>
+      </el-timeline-item>
+      <el-timeline-item timestamp="2018/4/3" placement="top">
+        <el-card>
+          <h4>更新 Github 模板</h4>
+          <p>王小虎 提交于 2018/4/3 20:46</p>
+        </el-card>
+      </el-timeline-item>
+      <el-timeline-item timestamp="2018/4/2" placement="top">
+        <el-card>
+          <h4>更新 Github 模板</h4>
+          <p>王小虎 提交于 2018/4/2 20:46</p>
+        </el-card>
+      </el-timeline-item>
+    </el-timeline>
   </div>
 </template>
 
