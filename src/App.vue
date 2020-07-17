ed2aa526314977acf5743eec26c3bbbaa760113e
jinyanwei <jinyanwei@onetarget.cn>
2020-07-17 11:39:02 +0800

    notification 使用html片段

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <el-button plain @click="open">偏移的消息</el-button>
+    <el-button plain @click="open">使用HTML片段</el-button>
   </div>
 </template>
 
@@ -13,10 +13,9 @@
         methods: {
             open() {
                 this.$notify({
-                    title: "偏移",
-                    message: "这是一条带有偏移的提示消息",
-                    offset: 300,
-                    position: "bottom-right"
+                    title: "html片段",
+                    dangerouslyUseHTMLString: true,
+                    message: "<strong>这是<i>HTML</i>片段</strong>"
                 })
             },
         }
