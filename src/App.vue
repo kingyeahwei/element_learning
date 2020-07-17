aef4dcbf3332ebba8a8b7de99e0c55775edb17c2
jinyanwei <jinyanwei@onetarget.cn>
2020-07-17 10:15:22 +0800

    message 文字居中

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <template>
   <div id="app">
-    <el-button :plain="true" @click="open2">成功</el-button>
-    <el-button :plain="true" @click="open3">警告</el-button>
-    <el-button :plain="true" @click="open1">消息</el-button>
-    <el-button :plain="true" @click="open4">错误</el-button>
+    <el-button :plain="true" @click="openCenter">文字居中</el-button>
   </div>
 </template>
 
@@ -16,23 +13,11 @@
             }
         },
         methods: {
-            open1() {
-                this.$message.info("这是一条消息提示");
-            },
-            open2() {
+            openCenter() {
                 this.$message({
-                    message: "恭喜你,这是一条成功消息",
-                    type: "success"
+                    message: "居中的文字",
+                    center: true
                 })
-            },
-            open3() {
-                this.$message({
-                    message: "警告偶,这是一条警告消息",
-                    type: "warning"
-                })
-            },
-            open4() {
-                this.$message.error("错了,这是一条错误消息")
             }
         }
     }
