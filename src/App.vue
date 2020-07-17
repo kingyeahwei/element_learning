925cf7facf77d6d4cabe7d65b868eb83f240b7ce
jinyanwei <jinyanwei@onetarget.cn>
2020-07-17 18:39:44 +0800

    card 简单卡片

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <div id="app">
-    <el-card class="box-card">
-      <div slot="header" class="clearfix">
-        <span>卡片名称</span>
-        <el-button style="float: right; padding: 3px 0;" type="text">操作按钮</el-button>
-      </div>
+    <el-card>
       <div v-for="o in 4" :key="o" class="text item">
         {{"列表内容" + o}}
       </div>
