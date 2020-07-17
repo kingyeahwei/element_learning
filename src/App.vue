d108ecc2982e6249fd9c9e4721859f91045e109c
jinyanwei <jinyanwei@onetarget.cn>
2020-07-17 10:04:46 +0800

    message 可关闭

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
-    <el-button :plain="true" @click="open">打开消息提示</el-button>
-    <el-button :plain="true" @click="openVn">Vnode</el-button>
+    <el-button :plain="true" @click="open1">消息</el-button>
+    <el-button :plain="true" @click="open2">成功</el-button>
+    <el-button :plain="true" @click="open3">警告</el-button>
+    <el-button :plain="true" @click="open4">错误</el-button>
   </div>
 </template>
 
@@ -13,16 +15,31 @@
             }
         },
         methods: {
-            open() {
-                this.$message("这是一条消息提示");
-            },
-            openVn() {
-                const h = this.$createElement;
+            open1() {
                 this.$message({
-                    message: h("p", null, [
-                        h("span", null, "内容可以是"),
-                        h("i", {style: "color: teal"}, "VNode")
-                    ])
+                    showClose: true,
+                    message: "这是一条消息提示"
+                })
+            },
+            open2() {
+                this.$message({
+                    showClose: true,
+                    message: "恭喜你, 这是一条成功消息",
+                    type: "success"
+                })
+            },
+            open3() {
+                this.$message({
+                    showClose: true,
+                    message: "警告哦,还是一条警告消息",
+                    type: "warning"
+                })
+            },
+            open4() {
+                this.$message({
+                    showClose: true,
+                    message: "错了哦, 这是一条错误消息",
+                    type: "error"
                 })
             }
         }
