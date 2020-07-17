2a0390bf8f079f77e122757275f18f07e838877b
jinyanwei <jinyanwei@onetarget.cn>
2020-07-17 10:43:24 +0800

    messagebox 提交内容

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <el-button type="text" @click="open">点击打开message box</el-button>
+    <el-button type="text" @click="open">点击打开 message box</el-button>
   </div>
 </template>
 
@@ -12,19 +12,22 @@
         },
         methods: {
             open() {
-                this.$confirm("此操作将永久删除文件,是否继续?", "提示", {
+                this.$prompt("请输入邮箱", "提示", {
                     confirmButtonText: "确定",
                     cancelButtonText: "取消",
-                    type: "warning"
-                }).then(() => {
-                    this.$message({
-                        type: "success",
-                        message: "删除成功"
+                    inputPattern: /[\w!#$%&'*+/=?^_`{|}~-]+(?:\.[\w!#$%&'*+/=?^_`{|}~-]+)*@(?:[\w](?:[\w-]*[\w])?\.)+[\w](?:[\w-]*[\w])?/,
+                    inputErrorMessage: "邮箱格式不正确"
+                })
+                    .then((value) => {
+                        this.$message({
+                            type: "success",
+                            message: "你的邮箱是: " + value
+                        })
                     })
-                }).catch(() => {
+                .catch(() => {
                     this.$message({
                         type: "info",
-                        message: "取消删除"
+                        message: "取消输入"
                     })
                 })
             }
