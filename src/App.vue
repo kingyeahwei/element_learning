fd6af1c4d3d535a2d448476e00c165ccd2a140fb
jinyanwei <jinyanwei@live.com>
2020-07-18 00:15:29 +0800

    timeline 自定义节点内容

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,18 @@
 <template>
   <div id="app">
     <div class="block">
-      <div class="radio">
-        排序:
-        <el-radio-group v-model="reverse">
-          <el-radio :label="true">倒序</el-radio>
-          <el-radio :label="false">正序</el-radio>
-        </el-radio-group>
-
-        <el-timeline :reverse="reverse">
-          <el-timeline-item
-            v-for="(activity, index) in activities"
-            :key="index"
-            :timestamp="activity.timestamp">
-            {{activity.content}}
-          </el-timeline-item>
-        </el-timeline>
-
-      </div>
+      <el-timeline>
+        <el-timeline-item
+          v-for="(activity,index) in activities"
+          :key="index"
+          :icon="activity.icon"
+          :type="activity.type"
+          :color="activity.color"
+          :size="activity.size"
+          :timestamp="activity.timestamp">
+          {{activity.content}}
+        </el-timeline-item>
+      </el-timeline>
     </div>
   </div>
 </template>
@@ -30,14 +25,22 @@
         reverse: true,
         activities: [
           {
-            content: '活动按期开始',
-            timestamp: '2018-04-15'
+            content: '支持使用图标',
+            timestamp: '2018-04-12 20:46',
+            size: 'large',
+            type: 'primary',
+            icon: 'el-icon-more'
           }, {
-            content: '通过审核',
-            timestamp: '2018-04-13'
+            content: '支持自定义颜色',
+            timestamp: '2018-04-03 20:46',
+            color: '#0bbd87'
           }, {
-            content: '创建成功',
-            timestamp: '2018-04-11'
+            content: '支持自定义尺寸',
+            timestamp: '2018-04-03 20:46',
+            size: 'large'
+          }, {
+            content: '默认样式的节点',
+            timestamp: '2018-04-03 20:46'
           }
         ]
       }
