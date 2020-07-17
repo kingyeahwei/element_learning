b770c25728e05444e73bc5f048373d9f59d82479
jinyanwei <jinyanwei@live.com>
2020-07-18 00:32:53 +0800

    divider 垂直分割

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <template>
   <div id="app">
-    <span>头上一片晴天，心中一个想念</span>
-    <el-divider content-position="left">少年包青天</el-divider>
-    <span>头上一片晴天，心中一个想念</span>
-    <el-divider><i class="el-icon-mobile-phone"></i></el-divider>
-    <span>头上一片晴天，心中一个想念</span>
-    <el-divider content-position="right">阿里云</el-divider>
+    <span>雨纷纷</span>
+    <el-divider direction="vertical"></el-divider>
+    <span>旧故里</span>
+    <el-divider direction="vertical"></el-divider>
+    <span>草木深</span>
   </div>
 </template>
 
