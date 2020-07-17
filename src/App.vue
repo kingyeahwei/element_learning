5dba4bfdf1e26878c88f2bedf25a62783fbb3a7d
jinyanwei <jinyanwei@onetarget.cn>
2020-07-17 16:01:46 +0800

    steps 带图标的步骤条

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <template>
   <div id="app">
-    <el-steps :active="2" align-center finish-status="error">
-      <el-step title="步骤1" description="走上社会发斯蒂芬斯蒂芬地方"></el-step>
-      <el-step title="步骤2" description="走上社会发斯蒂芬斯蒂芬地方"></el-step>
-      <el-step title="步骤3" description="走上社会发斯蒂芬斯蒂芬地方"></el-step>
-      <el-step title="步骤4" description="走上社会发斯蒂芬斯蒂芬地方"></el-step>
+    <el-steps :active="1">
+      <el-step title="步骤1" icon="el-icon-edit"></el-step>
+      <el-step title="步骤2" icon="el-icon-upload"></el-step>
+      <el-step title="步骤3" icon="el-icon-picture"></el-step>
     </el-steps>
   </div>
 </template>
