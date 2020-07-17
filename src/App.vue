44f20ff4eb730b733f86e9b2f76daf4916eaf033
jinyanwei <jinyanwei@live.com>
2020-07-18 01:11:55 +0800

    image 加载失败

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div id="app">
-    <div class="block">
-      <span class="demonstration">默认</span>
-      <el-image :src="src"></el-image>
-    </div>
-    <div class="block">
-      <span class="demonstration">自定义</span>
-      <el-image :src="src">
-        <div slot="placeholder" class="image-slot">
-          加载中 <span class="dot">...</span>
-        </div>
-      </el-image>
+    <div class="demo-image-error">
+      <div class="block">
+        <span>默认</span>
+        <el-image></el-image>
+      </div>
+      <div class="block">
+        <span>自定义</span>
+        <el-image>
+          <div slot="error" class="image-slot">
+            <i class="el-icon-picture-outline"></i>
+          </div>
+        </el-image>
+      </div>
     </div>
   </div>
 </template>
@@ -20,7 +22,6 @@
     name: "app",
     data() {
       return {
-        src: 'https://cube.elemecdn.com/6/94/4d3ea53c084bad6931a56d5158a48jpeg.jpeg'
       }
     },
     methods: {
