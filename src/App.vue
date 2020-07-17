6dd7b674eb04f4cdf6abdff1136508cba5dcb8cd
jinyanwei <jinyanwei@live.com>
2020-07-18 00:28:46 +0800

    divider 基础用法

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,8 @@
 <template>
   <div id="app">
-    <el-timeline>
-      <el-timeline-item timestamp="2018/4/12" placement="top">
-        <el-card>
-          <h4>更新 Github 模板</h4>
-          <p>王小虎 提交于 2018/4/12 20:46</p>
-        </el-card>
-      </el-timeline-item>
-      <el-timeline-item timestamp="2018/4/3" placement="top">
-        <el-card>
-          <h4>更新 Github 模板</h4>
-          <p>王小虎 提交于 2018/4/3 20:46</p>
-        </el-card>
-      </el-timeline-item>
-      <el-timeline-item timestamp="2018/4/2" placement="top">
-        <el-card>
-          <h4>更新 Github 模板</h4>
-          <p>王小虎 提交于 2018/4/2 20:46</p>
-        </el-card>
-      </el-timeline-item>
-    </el-timeline>
+    <span>青春是一个短暂的美梦, 当你醒来时, 它早已消失无踪</span>
+    <el-divider></el-divider>
+    <span>青春是一个短暂的美梦, 当你醒来时, 它早已消失无踪</span>
   </div>
 </template>
 
@@ -28,33 +11,9 @@
     name: "app",
     data() {
       return {
-        reverse: true,
-        activities: [
-          {
-            content: '支持使用图标',
-            timestamp: '2018-04-12 20:46',
-            size: 'large',
-            type: 'primary',
-            icon: 'el-icon-more'
-          }, {
-            content: '支持自定义颜色',
-            timestamp: '2018-04-03 20:46',
-            color: '#0bbd87'
-          }, {
-            content: '支持自定义尺寸',
-            timestamp: '2018-04-03 20:46',
-            size: 'large'
-          }, {
-            content: '默认样式的节点',
-            timestamp: '2018-04-03 20:46'
-          }
-        ]
       }
     },
     methods: {
-      handleChange(value) {
-        console.log(value)
-      }
     }
   }
 </script>
