9fdb0de9e35918b03e7fb3349920d377596a8019
jinyanwei <jinyanwei@onetarget.cn>
2020-07-17 11:01:44 +0800

    messagebox 使用html片段

--- src/App.vue
+++ src/App.vue
@@ -12,37 +12,8 @@
         },
         methods: {
             open() {
-                const h = this.$createElement;
-                this.$msgbox({
-                    title: "消息",
-                    message: h("p", null, [
-                        h("span", null, "内容可以是"),
-                        h("i", {style: "color: teal"}, "VNode")
-                    ]),
-                    showCancelButton: true,
-                    confirmButtonText: "确定",
-                    cancelButtonText: "取消",
-                    beforeClose: (action, instance, done) => {
-                        if (action == "confirm") {
-                            instance.confirmButtonLoading = true;
-                            instance.confirmButtonText = "执行中...";
-                            setTimeout(() => {
-                                done();
-                                setTimeout(() => {
-                                    instance.confirmButtonLoading = false;
-                                }, 300);
-                            }, 3000);
-                        } else {
-                            done();
-                        }
-                    }
-                }).then((action) => {
-                    this.$message({
-                        type: "info",
-                        message: "action:" + action
-                    })
-                }).catch((action) => {
-                    console.log(action)
+                this.$alert("<strong>这是<i>HMTL</i>片段</strong>", "HTML片段", {
+                    dangerouslyUseHTMLString: true
                 })
             }
         }
