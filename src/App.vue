5834bbec77e792c054d4756c01a91d1389b58f51
jinyanwei <jinyanwei@live.com>
2020-07-18 00:06:44 +0800

    timeline 基础用法

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,24 @@
 <template>
   <div id="app">
-    <el-collapse v-model="activename" accordion>
-      <el-collapse-item name="1">
-        <template slot="title" >
-          一致性 <i class="header-icon el-icon-info"></i>
-        </template>
-        <div>与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；</div>
-        <div>在界面中一致：所有的元素和结构需保持一致，比如：设计样式、图标和文本、元素的位置等。</div>
-      </el-collapse-item>
-      <el-collapse-item title="反馈" name="2">
-        <div>控制反馈：通过界面样式和交互动效让用户可以清晰的感知自己的操作；</div>
-        <div>页面反馈：操作后，通过页面元素的变化清晰地展现当前状态。</div>
-      </el-collapse-item>
-      <el-collapse-item title="效率" name="3">
-        <div>简化流程：设计简洁直观的操作流程；</div>
-        <div>清晰明确：语言表达清晰且表意明确，让用户快速理解进而作出决策；</div>
-        <div>帮助用户识别：界面简单直白，让用户快速识别而非回忆，减少用户记忆负担。</div>
-      </el-collapse-item>
-      <el-collapse-item title="可控" name="4">
-        <div>用户决策：根据场景可给予用户操作建议或安全提示，但不能代替用户进行决策；</div>
-        <div>结果可控：用户可以自由的进行操作，包括撤销、回退和终止当前操作等。</div>
-      </el-collapse-item>
-    </el-collapse>
+    <div class="block">
+      <div class="radio">
+        排序:
+        <el-radio-group v-model="reverse">
+          <el-radio :label="true">倒序</el-radio>
+          <el-radio :label="false">正序</el-radio>
+        </el-radio-group>
+
+        <el-timeline :reverse="reverse">
+          <el-timeline-item
+            v-for="(activity, index) in activities"
+            :key="index"
+            :timestamp="activity.timestamp">
+            {{activity.content}}
+          </el-timeline-item>
+        </el-timeline>
+
+      </div>
+    </div>
   </div>
 </template>
 
@@ -30,7 +27,19 @@
     name: "app",
     data() {
       return {
-        activename: "1"
+        reverse: true,
+        activities: [
+          {
+            content: '活动按期开始',
+            timestamp: '2018-04-15'
+          }, {
+            content: '通过审核',
+            timestamp: '2018-04-13'
+          }, {
+            content: '创建成功',
+            timestamp: '2018-04-11'
+          }
+        ]
       }
     },
     methods: {
