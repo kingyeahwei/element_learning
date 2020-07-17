505d1f2347bd35f992393087f591390d35356ff6
jinyanwei <jinyanwei@live.com>
2020-07-17 23:42:35 +0800

    collapse 基础用法

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,24 @@
 <template>
   <div id="app">
-    <el-carousel height="200px" direction="vertical" :autoplay="false">
-      <el-carousel-item v-for="item in 6" :key="item">
-        <h3 class="medium">{{item}}</h3>
-      </el-carousel-item>
-    </el-carousel>
+    <el-collapse v-model="activeNames" @change="handleChange">
+      <el-collapse-item title="一致性" name="1">
+        <div>与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；</div>
+        <div>在界面中一致：所有的元素和结构需保持一致，比如：设计样式、图标和文本、元素的位置等。</div>
+      </el-collapse-item>
+      <el-collapse-item title="反馈" name="2">
+        <div>控制反馈：通过界面样式和交互动效让用户可以清晰的感知自己的操作；</div>
+        <div>页面反馈：操作后，通过页面元素的变化清晰地展现当前状态。</div>
+      </el-collapse-item>
+      <el-collapse-item title="效率" name="3">
+        <div>简化流程：设计简洁直观的操作流程；</div>
+        <div>清晰明确：语言表达清晰且表意明确，让用户快速理解进而作出决策；</div>
+        <div>帮助用户识别：界面简单直白，让用户快速识别而非回忆，减少用户记忆负担。</div>
+      </el-collapse-item>
+      <el-collapse-item title="可控" name="4">
+        <div>用户决策：根据场景可给予用户操作建议或安全提示，但不能代替用户进行决策；</div>
+        <div>结果可控：用户可以自由的进行操作，包括撤销、回退和终止当前操作等。</div>
+      </el-collapse-item>
+    </el-collapse>
   </div>
 </template>
 
@@ -12,27 +26,17 @@
   export default {
     name: "app",
     data() {
-      return {}
+      return {
+        activeNames: ["1"]
+      }
     },
-    methods: {}
+    methods: {
+      handleChange(value) {
+        console.log(value)
+      }
+    }
   }
 </script>
 
 <style lang="less">
-  .el-carousel__item h3 {
-    color: #475669;
-    font-size: 14px;
-    opacity: 0.75;
-    line-height: 200px;
-    margin: 0;
-    text-align: center;
-  }
-
-  .el-carousel__item:nth-child(2n) {
-    background-color: #99a9bf;
-  }
-
-  .el-carousel__item:nth-child(2n + 1) {
-    background-color: #d3dce6;
-  }
 </style>
