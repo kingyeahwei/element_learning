e68cf54c708d980715182fee009c61e757895534
jinyanwei <jinyanwei@onetarget.cn>
2020-07-17 16:55:42 +0800

    dialog 居中布局

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
 <template>
   <div id="app">
-    <el-button type="text" @click="outerVisible = true">点击打开外层dialog</el-button>
-
-    <el-dialog title="外层dialog" :visible.sync="outerVisible" width="80%">
-      <el-dialog
-        title="内层dialog"
-        width="30%"
-        :visible.sync="innerVisible"
-        append-to-body
-      ></el-dialog>
-      <div slot="footer" class="dialog-footer">
-        <el-button @click="outerVisible = false">取消</el-button>
-        <el-button type="primary" @click="innerVisible = true">打开内层dialog</el-button>
+    <el-button type="text" @click="centerDialogVisible = true">点击打开dialog</el-button>
+    <el-dialog
+      title="提示"
+      :visible.sync="centerDialogVisible"
+      width="30%"
+      center>
+      <div style="text-align: center">
+        <span>需要注意的是内容是默认不居中的</span>
       </div>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="centerDialogVisible = false">取消</el-button>
+        <el-button type="primary" @click="centerDialogVisible = false">确定</el-button>
+      </span>
     </el-dialog>
   </div>
 </template>
@@ -22,8 +22,7 @@
         name: "app",
         data() {
             return {
-                outerVisible: false,
-                innerVisible: false
+                centerDialogVisible: false
             }
         },
         methods: {
