2563cc46cd358696c96895b57337726fd2fafa88
jinyanwei <jinyanwei@live.com>
2020-07-17 23:09:42 +0800

    carousel 基础用法

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <template>
   <div id="app">
-    <el-row :gutter="12">
-      <el-col :span="8">
-        <el-card shadow="always">总是显示</el-card>
-      </el-col>
-      <el-col :span="8">
-        <el-card shadow="hover">鼠标悬浮时显示</el-card>
-      </el-col>
-      <el-col :span="8">
-        <el-card shadow="never">从不显示</el-card>
-      </el-col>
-    </el-row>
+    <div class="block">
+      <span class="demonstration">默认 Hover 指示器触发</span>
+      <el-carousel height="150px">
+        <el-carousel-item v-for="item in 4" :key="item">
+          <h3 class="small">{{ item }}</h3>
+        </el-carousel-item>
+      </el-carousel>
+    </div>
+    <div class="block">
+      <span class="demonstration">Click 指示器触发</span>
+      <el-carousel trigger="click" height="150px">
+        <el-carousel-item v-for="item in 4" :key="item">
+          <h3 class="small">{{ item }}</h3>
+        </el-carousel-item>
+      </el-carousel>
+    </div>
   </div>
 </template>
 
@@ -18,12 +23,26 @@
   export default {
     name: "app",
     data() {
-      return {
-      }
+      return {}
     },
     methods: {}
   }
 </script>
 
 <style lang="less">
+  .el-carousel__item h3 {
+    color: #475669;
+    font-size: 14px;
+    opacity: 0.75;
+    line-height: 150px;
+    margin: 0;
+  }
+
+  .el-carousel__item:nth-child(2n) {
+    background-color: #99a9bf;
+  }
+
+  .el-carousel__item:nth-child(2n+1) {
+    background-color: #d3dce6;
+  }
 </style>
