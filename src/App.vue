067654a4b7f7aa4fd3d7646b941023448d90ad65
jinyanwei <jinyanwei@onetarget.cn>
2020-07-17 15:05:44 +0800

    dropdown 触发对象

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,25 @@
 <template>
   <div id="app">
     <el-dropdown>
-      <span class="el-dropdown-link">
-        下拉菜单<i class="el-icon-arrow-down el-icon--right"></i>
-      </span>
+      <el-button type="primary">
+        更多菜单 <i class="el-icon-arrow-down el-icon--right"></i>
+      </el-button>
       <el-dropdown-menu slot="dropdown">
         <el-dropdown-item>黄金糕</el-dropdown-item>
         <el-dropdown-item>狮子头</el-dropdown-item>
         <el-dropdown-item>螺蛳粉</el-dropdown-item>
-        <el-dropdown-item disabled>双皮奶</el-dropdown-item>
-        <el-dropdown-item divided>蚵仔煎</el-dropdown-item>
+        <el-dropdown-item>双品店</el-dropdown-item>
+        <el-dropdown-item>蚵仔煎</el-dropdown-item>
+      </el-dropdown-menu>
+    </el-dropdown>
+    <el-dropdown split-button type="primary" @click="handleClick">
+      更多菜单
+      <el-dropdown-menu slot="dropdown">
+        <el-dropdown-item>黄金糕</el-dropdown-item>
+        <el-dropdown-item>狮子头</el-dropdown-item>
+        <el-dropdown-item>螺蛳粉</el-dropdown-item>
+        <el-dropdown-item>双品店</el-dropdown-item>
+        <el-dropdown-item>蚵仔煎</el-dropdown-item>
       </el-dropdown-menu>
     </el-dropdown>
   </div>
@@ -23,6 +33,9 @@
             }
         },
         methods: {
+            handleClick() {
+                alert("button click")
+            }
         }
     }
 </script>
