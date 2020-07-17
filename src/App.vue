fa1705d2d1e993473b4caa4ec86885f21e22f0f3
jinyanwei <jinyanwei@onetarget.cn>
2020-07-17 15:44:30 +0800

    dropdown 不同尺寸

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,43 @@
 <template>
   <div id="app">
-    <el-dropdown @command="handleCommand">
-      <span class="el-dropdown-link">
-        下拉菜单 <i class="el-icon-arrow-down el-icon--right"></i>
-      </span>
+    <el-dropdown split-button type="primary">
+      默认尺寸
       <el-dropdown-menu slot="dropdown">
-        <el-dropdown-item command="a">华金糕</el-dropdown-item>
-        <el-dropdown-item command="b">狮子头</el-dropdown-item>
-        <el-dropdown-item command="c">螺蛳粉</el-dropdown-item>
-        <el-dropdown-item command="d">蒜片</el-dropdown-item>
-        <el-dropdown-item command="e">蚵仔煎</el-dropdown-item>
+        <el-dropdown-item>黄金糕</el-dropdown-item>
+        <el-dropdown-item>狮子头</el-dropdown-item>
+        <el-dropdown-item>螺蛳粉</el-dropdown-item>
+        <el-dropdown-item>双皮奶</el-dropdown-item>
+        <el-dropdown-item>蚵仔煎</el-dropdown-item>
+      </el-dropdown-menu>
+    </el-dropdown>
+    <el-dropdown split-button type="primary" size="medium">
+      默认尺寸
+      <el-dropdown-menu slot="dropdown">
+        <el-dropdown-item>黄金糕</el-dropdown-item>
+        <el-dropdown-item>狮子头</el-dropdown-item>
+        <el-dropdown-item>螺蛳粉</el-dropdown-item>
+        <el-dropdown-item>双皮奶</el-dropdown-item>
+        <el-dropdown-item>蚵仔煎</el-dropdown-item>
+      </el-dropdown-menu>
+    </el-dropdown>
+    <el-dropdown split-button type="primary" size="small">
+      默认尺寸
+      <el-dropdown-menu slot="dropdown">
+        <el-dropdown-item>黄金糕</el-dropdown-item>
+        <el-dropdown-item>狮子头</el-dropdown-item>
+        <el-dropdown-item>螺蛳粉</el-dropdown-item>
+        <el-dropdown-item>双皮奶</el-dropdown-item>
+        <el-dropdown-item>蚵仔煎</el-dropdown-item>
+      </el-dropdown-menu>
+    </el-dropdown>
+    <el-dropdown split-button type="primary" size="mini">
+      默认尺寸
+      <el-dropdown-menu slot="dropdown">
+        <el-dropdown-item>黄金糕</el-dropdown-item>
+        <el-dropdown-item>狮子头</el-dropdown-item>
+        <el-dropdown-item>螺蛳粉</el-dropdown-item>
+        <el-dropdown-item>双皮奶</el-dropdown-item>
+        <el-dropdown-item>蚵仔煎</el-dropdown-item>
       </el-dropdown-menu>
     </el-dropdown>
   </div>
@@ -23,20 +51,9 @@
             }
         },
         methods: {
-            handleCommand(command) {
-                console.log(command)
-                this.$message("click on item" + command);
-            }
         }
     }
 </script>
 
 <style lang="less">
-  .el-dropdown-link {
-    cursor: pointer;
-    color: blue;
-  }
-  .el-icon-arrow-down {
-    color: blue;
-  }
 </style>
