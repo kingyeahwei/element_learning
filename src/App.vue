9e15e691a51506c8929c9af2e6d16c2dc1002cb0
jinyanwei <jinyanwei@onetarget.cn>
2020-07-17 10:12:57 +0800

    message 不同状态

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
-    <el-button :plain="true" @click="open1">消息</el-button>
     <el-button :plain="true" @click="open2">成功</el-button>
     <el-button :plain="true" @click="open3">警告</el-button>
+    <el-button :plain="true" @click="open1">消息</el-button>
     <el-button :plain="true" @click="open4">错误</el-button>
   </div>
 </template>
@@ -12,35 +12,27 @@
         name: "app",
         data() {
             return {
+
             }
         },
         methods: {
             open1() {
-                this.$message({
-                    showClose: true,
-                    message: "这是一条消息提示"
-                })
+                this.$message.info("这是一条消息提示");
             },
             open2() {
                 this.$message({
-                    showClose: true,
-                    message: "恭喜你, 这是一条成功消息",
+                    message: "恭喜你,这是一条成功消息",
                     type: "success"
                 })
             },
             open3() {
                 this.$message({
-                    showClose: true,
-                    message: "警告哦,还是一条警告消息",
+                    message: "警告偶,这是一条警告消息",
                     type: "warning"
                 })
             },
             open4() {
-                this.$message({
-                    showClose: true,
-                    message: "错了哦, 这是一条错误消息",
-                    type: "error"
-                })
+                this.$message.error("错了,这是一条错误消息")
             }
         }
     }
