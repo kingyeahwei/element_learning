594404c68d4be739b3e2387fed822948d134babe
jinyanwei <jinyanwei@onetarget.cn>
2020-07-17 16:09:12 +0800

    steps 简洁风格的步骤条

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
   <div id="app">
-    <div style="height: 300px">
-      <el-steps :active="1" direction="vertical">
-        <el-step title="步骤1" icon="el-icon-edit"></el-step>
-        <el-step title="步骤2" icon="el-icon-upload"></el-step>
-        <el-step title="步骤3" icon="el-icon-picture"></el-step>
-      </el-steps>
-    </div>
+    <el-steps :active="1" simple>
+      <el-step title="步骤1" icon="el-icon-edit"></el-step>
+      <el-step title="步骤2" icon="el-icon-upload"></el-step>
+      <el-step title="步骤3" icon="el-icon-picture"></el-step>
+    </el-steps>
+
+    <el-steps :active="1" finish-status="success" simple style="margin-top: 20px;">
+      <el-step title="步骤 1"></el-step>
+      <el-step title="步骤 2"></el-step>
+      <el-step title="步骤 3"></el-step>
+    </el-steps>
   </div>
 </template>
 
