afb75b8562a4ae196addcf03640bf8c75eb25376
jinyanwei <jinyanwei@onetarget.cn>
2020-07-17 16:46:43 +0800

    dialog 嵌套dialog

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,17 @@
 <template>
   <div id="app">
-    <el-button type="text" @click="dialogTableVisible = true">打开嵌套表格的dialog</el-button>
-    <el-dialog title="收货地址" :visible.sync="dialogTableVisible">
-      <el-table :data="gridData">
-        <el-table-column property="date" label="日期" width="150"></el-table-column>
-        <el-table-column property="name" label="姓名" width="200"></el-table-column>
-        <el-table-column property="address" label="地址"></el-table-column>
-      </el-table>
-    </el-dialog>
+    <el-button type="text" @click="outerVisible = true">点击打开外层dialog</el-button>
 
-    <el-button type="text" @click="dialogFormVisible = true">打开嵌套表单的dialog</el-button>
-    <el-dialog title="收货地址" :visible.sync="dialogFormVisible">
-      <el-form :model="form">
-        <el-form-item label="活动名称" :label-width="formLabelWidth">
-          <el-input v-model="form.name" auto-complete="off"></el-input>
-        </el-form-item>
-        <el-form-item label="活动区域" :label-width="formLabelWidth">
-          <el-select v-model="form.region" placeholder="请选择活动区域">
-            <el-option label="区域1" value="shanghai"></el-option>
-            <el-option label="区域2" value="beijing"></el-option>
-          </el-select>
-        </el-form-item>
-      </el-form>
+    <el-dialog title="外层dialog" :visible.sync="outerVisible" width="80%">
+      <el-dialog
+        title="内层dialog"
+        width="30%"
+        :visible.sync="innerVisible"
+        append-to-body
+      ></el-dialog>
       <div slot="footer" class="dialog-footer">
-        <el-button @click="dialogFormVisible = false">取消</el-button>
-        <el-button @click="dialogFormVisible = false" type="primary">确定</el-button>
+        <el-button @click="outerVisible = false">取消</el-button>
+        <el-button type="primary" @click="innerVisible = true">打开内层dialog</el-button>
       </div>
     </el-dialog>
   </div>
@@ -35,48 +22,11 @@
         name: "app",
         data() {
             return {
-                dialogTableVisible: false,
-                dialogFormVisible: false,
-                gridData: [
-                    {
-                        date: '2016-05-02',
-                        name: '王小虎',
-                        address: '上海市普陀区金沙江路 1518 弄'
-                    }, {
-                        date: '2016-05-04',
-                        name: '王小虎',
-                        address: '上海市普陀区金沙江路 1518 弄'
-                    }, {
-                        date: '2016-05-01',
-                        name: '王小虎',
-                        address: '上海市普陀区金沙江路 1518 弄'
-                    }, {
-                        date: '2016-05-03',
-                        name: '王小虎',
-                        address: '上海市普陀区金沙江路 1518 弄'
-                    }
-                ],
-                form: {
-                    name: "",
-                    region: "",
-                    date1: "",
-                    date2: "",
-                    delivery: false,
-                    type: [],
-                    resource: "",
-                    desc: ""
-                },
-                formLabelWidth: "120px"
+                outerVisible: false,
+                innerVisible: false
             }
         },
         methods: {
-            handleClose(done) {
-                this.$confirm("确认关闭").then(() => {
-                    done()
-                }).catch(() => {
-
-                })
-            }
         }
     }
 </script>
