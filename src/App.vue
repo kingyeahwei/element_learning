6b41d093a34712ce55492c80d2fd234779c9a63c
jinyanwei <jinyanwei@onetarget.cn>
2020-07-17 18:13:13 +0800

    popover 嵌套操作

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <div id="app"  style="margin: 200px 0 0 200px">
     <el-popover
-      placement="right"
-      width="400"
-      trigger="click">
-      <el-table :data="gridData">
-        <el-table-column width="150" property="date" label="日期"></el-table-column>
-        <el-table-column width="100" property="name" label="姓名"></el-table-column>
-        <el-table-column width="300" property="address" label="地址"></el-table-column>
-      </el-table>
-      <el-button slot="reference">click 激活</el-button>
+      placement="top"
+      width="160"
+      v-model="visible">
+      <p>这是一段内容这是一段内容确定要删除吗?</p>
+      <div style="text-align: right; margin: 0">
+        <el-button size="mini" type="text" @click="visible = false">取消</el-button>
+        <el-button type="primary" size="mini" @click="visible = false">确定</el-button>
+      </div>
+      <el-button slot="reference">删除</el-button>
     </el-popover>
   </div>
 </template>
@@ -19,25 +19,7 @@
         name: "app",
         data() {
             return {
-                gridData: [
-                    {
-                        date: '2016-05-02',
-                        name: '王小虎',
-                        address: '上海市普陀区金沙江路 1518 弄'
-                    }, {
-                        date: '2016-05-04',
-                        name: '王小虎',
-                        address: '上海市普陀区金沙江路 1518 弄'
-                    }, {
-                        date: '2016-05-01',
-                        name: '王小虎',
-                        address: '上海市普陀区金沙江路 1518 弄'
-                    }, {
-                        date: '2016-05-03',
-                        name: '王小虎',
-                        address: '上海市普陀区金沙江路 1518 弄'
-                    }
-                ]
+                visible: false
             }
         },
         methods: {
