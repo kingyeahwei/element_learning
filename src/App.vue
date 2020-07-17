598bc4cc29df2722d85df0293da0c53921f0ff06
jinyanwei <jinyanwei@onetarget.cn>
2020-07-17 14:58:26 +0800

    dropdown 基础用法

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <template>
   <div id="app">
-    <el-page-header @back="goBack" content="详情页面">
-      <template slot="title">title</template>
-      <template slot="content">content</template>
-    </el-page-header>
+    <el-dropdown>
+      <span class="el-dropdown-link">
+        下拉菜单<i class="el-icon-arrow-down el-icon--right"></i>
+      </span>
+      <el-dropdown-menu slot="dropdown">
+        <el-dropdown-item>黄金糕</el-dropdown-item>
+        <el-dropdown-item>狮子头</el-dropdown-item>
+        <el-dropdown-item>螺蛳粉</el-dropdown-item>
+        <el-dropdown-item disabled>双皮奶</el-dropdown-item>
+        <el-dropdown-item divided>蚵仔煎</el-dropdown-item>
+      </el-dropdown-menu>
+    </el-dropdown>
   </div>
 </template>
 
@@ -15,9 +23,6 @@
             }
         },
         methods: {
-            goBack() {
-                console.log("go back!!")
-            }
         }
     }
 </script>
